9168daa1787bc2e3bcd47a8befcba6e73c1991c2
강은빈 <kub0803@naver.com>
2020-10-26 09:20:34 +0900

    [Modify] 프로필 컴포넌트

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import * as S from '../styled/Profile/style';
+//프로필 컴포넌트
+import React from "react";
+import * as S from "../styled/Profile/style";
 
-const Profile = props => {
-    const { team, title, date } = props;
-    return (
-        <div>
-            <S.MainProjectTeam>
-              <span>{ team }</span>
-              <p>{ title }</p>
-              <span>{ date }</span>
-            </S.MainProjectTeam>
-        </div>
-    )
-}
+const Profile = (props) => {
+  const { name, email, produce, github } = props;
+/*   if(name === "팀"){
+
+  }
+ */
+  return (
+    <S.Profile>
+      <S.ProPhoto>
+        {/* 프로필 사진 */}
+        <div></div>
+        {/* 이름 / 깃허브 주소 / 자신 설명 */}
+        <span>{name}</span>
+        <span>{email}</span>
+      </S.ProPhoto>
+      <S.Produce>
+        <a href={github}>
+          깃허브로 이동
+        </a>
+        <p>{produce}</p>
+      </S.Produce>
+    </S.Profile>
+  );
+};
 
 export default Profile;
+
