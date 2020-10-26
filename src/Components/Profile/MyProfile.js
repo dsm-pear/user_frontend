e4687fcb173807aaa026282bb948ad3e74f91aff
강은빈 <kub0803@naver.com>
2020-10-26 09:21:08 +0900

    [Modify] 내가 보는 내 프로필

--- src/Components/Profile/MyProfile.js
+++ src/Components/Profile/MyProfile.js
@@ -1,46 +1,65 @@
+//내가 보는 내 프로필 수정 하기 누르기 전
+
 import React from "react";
-import * as M from "../styled/Profile/Mystyled";
 import * as S from "../styled/Profile/style";
 //import UserProfile from "./UserProfile";
+import Project from "./Project";
 import Profile from "./Profile";
+import Button from './Button';
 
 function MyProfile() {
   return (
     <S.Main>
-      <div>
-        <S.MainProfile>
-          {/* 좌측 프로필 */}
-          <S.Profile>
-            <S.ProPhoto>
-              {/* 프로필 사진 */}
-              <div></div>
-              {/* 이름 / 깃허브 주소 / 자신 설명 */}
-              <span>강은빈</span>
-              <span>kub9722@gmail.com</span>
-            </S.ProPhoto>
-            <S.Produce>
-              <a href="https://github.com/silverbeen">
-                https://github.com/silverbeen
-              </a>
-              <p>
-                안녕하세요. 대덕소프트웨어마이스터고등학교에 재학중인 강은빈
-                입니다.
-              </p>
-            </S.Produce>
-          </S.Profile>
+      <S.MainProfile>
+        {/* 좌측 프로필 */}
+        <S.Cover>
+          <Profile
+            name="강은빈"
+            email="kub9722@gmaeil.com"
+            produce="안녕하세요"
+            github="https://github.com/silverbeen"
+          />
 
           {/* 프로젝트 보여주는 곳 */}
           <S.Project>
             <S.PreProject>
-              {/* 개인 프로젝트 */}
-              <Profile team="개인" title="안녕" date="10" />
+              <Project
+                team="동아리"
+                title="1301 강은빈입니다"
+                date="20.10.26"
+              />
+              <Project
+                team="동아리"
+                title="1301 강은빈입니다"
+                date="20.10.26"
+              />
+              <Project
+                team="동아리"
+                title="1301 강은빈입니다"
+                date="20.10.26"
+              />
+              <Project
+                team="동아리"
+                title="1301 강은빈입니다"
+                date="20.10.26"
+              />
+              <Project
+                team="동아리"
+                title="1301 강은빈입니다"
+                date="20.10.26"
+              />
+              <Project
+                team="동아리"
+                title="1301 강은빈입니다"
+                date="20.10.26"
+              />
               {/* 밑에 더보기 / 숫자 */}
               <S.ProNum></S.ProNum>
             </S.PreProject>
           </S.Project>
-        </S.MainProfile>
-      </div>
-      <M.Modify></M.Modify>
+        </S.Cover>
+        <Button text="수정"/>
+      </S.MainProfile>
     </S.Main>
   );
 }
