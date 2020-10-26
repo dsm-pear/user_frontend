453a3570f15d0f1533aaa69e9b7ad13f66f8b51f
Hyeonyee <jgh10070@naver.com>
2020-10-26 22:05:34 +0900

    ReportWriting end

---
+++ src/Components/ReportWriting/ReportWriting.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import * as S from '../styled/ReportWriting/style';
+
+const ReportWriting = () => {
+    return (
+        <S.Main>
+            <div>
+               {/*만약 styled-components를 각각의 컴포넌트로 관리 해줘야 한다면 코드를 짜는 컴포넌트에서는 틀을 어떻게 잡아야 할까..?*/}
+               {/*style 컴포넌트 관리 법*/} 
+            </div>
+        </S.Main>
+    );
+}
+
+export default ReportWriting;
