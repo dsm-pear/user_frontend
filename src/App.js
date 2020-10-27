e980b9791812e1e6e989a690d9b0944a18dc847f
강은빈 <kub0803@naver.com>
2020-10-27 22:47:22 +0900

    Auto stash before merge of "feature/header" and "feature/ViewReport"

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import React from "react";
-import React from "react";
-import Header from "./components/Main/Header";
+import React from 'react';
+import Header from './components/Main/Header';
 import { BrowserRouter, Route } from "react-router-dom";
 import GlobalFonts from "./GlobalStyle";
 import Login from "./Components/Login/Login.js";
@@ -10,9 +9,9 @@ import MyProfile from "./Components/Profile/MyProfile";
 function App() {
   return (
     <>
-      <GlobalFonts />
-      <Header />
-      <BrowserRouter>
+    <GlobalFonts />
+    <Header/>
+    <BrowserRouter>
         <Route path="/login" component={Login} exact />
         <Route path="/UserProfile" component={UserProfile} exact />
         <Route path="/MyProfile" component={MyProfile} exact />
