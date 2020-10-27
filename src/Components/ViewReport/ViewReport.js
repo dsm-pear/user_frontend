debdcb2bd49af33acf4682fa49a0db723ba80cd5
강은빈 <kub0803@naver.com>
2020-10-27 22:11:32 +0900

    [Add] 보고서 보기 메인

--- src/Components/ViewReport/ViewReport.js
+++ src/Components/ViewReport/ViewReport.js
@@ -2,7 +2,11 @@ import React from "react";
 import * as R from "../styled/ViewReport/style";
 
 function ViewReport() {
-  return <R.Main></R.Main>;
+  return (
+    <R.Main>
+      <R.Cover></R.Cover>
+    </R.Main>
+  );
 }
 
 export default ViewReport;
