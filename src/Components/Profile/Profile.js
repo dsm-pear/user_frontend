591f05b732b53dc9c7dea532426489d418e00ea4
강은빈 <kub0803@naver.com>
2020-10-27 11:16:49 +0900

    [Modify] 버튼 클릭시 input 수정 가능

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -3,29 +3,35 @@ import React from "react";
 import * as S from "../styled/Profile/style";
 
 const Profile = (props) => {
-  const { name, email, produce, github } = props;
-/*   if(name === "팀"){
+  const { name, email, produce, github, text } = props;
 
-  }
- */
   return (
     <S.Profile>
       <S.ProPhoto>
         {/* 프로필 사진 */}
         <div></div>
         {/* 이름 / 깃허브 주소 / 자신 설명 */}
-        <span>{name}</span>
-        <span>{email}</span>
+        <input
+          type="email"
+          placeholder={name}
+          disabled={text === "저장" ? false : true}
+        />
+        <input
+          type="name"
+          placeholder={email}
+          disabled={text === "저장" ? false : true}
+        />
       </S.ProPhoto>
       <S.Produce>
-        <a href={github}>
-          깃허브로 이동
-        </a>
-        <p>{produce}</p>
+        <a href={github}>깃허브</a>
+        <inupt
+          type="text"
+          placeholder={produce}
+          disabled={text === "저장" ? false : true}
+        />
       </S.Produce>
     </S.Profile>
   );
 };
 
 export default Profile;
-
