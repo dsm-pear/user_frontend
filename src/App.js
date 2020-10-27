7662a8d73b1e783b44ea9ea768ca16d9abbdf981
강은빈 <kub0803@naver.com>
2020-10-27 22:44:17 +0900

    Auto stash before merge of "feature/header" and "feature/ViewReport"

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
+<<<<<<< Updated upstream
 import React from "react";
+=======
+import React from 'react';
+import Header from './components/Main/Header';
+>>>>>>> Stashed changes
 import { BrowserRouter, Route } from "react-router-dom";
 import GlobalFonts from "./GlobalStyle";
 import Login from "./Components/Login/Login.js";
 import UserProfile from "./Components/Profile/UserProfile";
 import MyProfile from "./Components/Profile/MyProfile";
+<<<<<<< Updated upstream
 import ViewReport from './Components/ViewReport/ViewReport';
+=======
+>>>>>>> Stashed changes
 
 
 function App() {
   return (
     <>
+<<<<<<< Updated upstream
       <GlobalFonts />
       <BrowserRouter>
         <Route path="/login" component={Login} exact />
@@ -18,6 +27,15 @@ function App() {
         <ViewReport/>
       </BrowserRouter>
 
+=======
+    <GlobalFonts />
+    <Header/>
+    <BrowserRouter>
+        <Route path="/login" component={Login} exact />
+        <Route path="/UserProfile" component={UserProfile} exact />
+        <Route path="/MyProfile" component={MyProfile} exact />
+      </BrowserRouter>
+>>>>>>> Stashed changes
     </>
   );
 }
