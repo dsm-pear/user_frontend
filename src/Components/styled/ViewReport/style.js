9655515bf5fa165eb4de5728a879e328e9e44ff3
강은빈 <kub0803@naver.com>
2020-10-27 22:11:54 +0900

    [Add] 보고서 보기 스타일

---
+++ src/Components/styled/ViewReport/style.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+import { MainBackground } from "../../../assets";
+
+const Main = styled.div`
+  height: 100vh;
+  background-image: url(${MainBackground});
+  background-size: 100vw 100vh;
+  background-repeat: no-repeat;
+
+`;
+
+const Cover = styled.div`
+  width: 
+`;
+
+export { Main, Cover };
