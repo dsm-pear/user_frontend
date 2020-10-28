e8b2af3ad95bd7e60916da464f2248a49c91a102
강은빈 <kub0803@naver.com>
2020-10-28 23:37:35 +0900

    [Modify]

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React from 'react';
-import Header from './components/Main/Header';
-import { BrowserRouter, Route } from "react-router-dom";
+import React from "react";
+import { Route, BrowserRouter } from "react-router-dom";
 import GlobalFonts from "./GlobalStyle";
-import Login from "./Components/Login/Login.js";
-import UserProfile from "./Components/Profile/UserProfile";
-import MyProfile from "./Components/Profile/MyProfile";
+import UserRouter from "./Routers/UserRouter";
+import ViewReport from './Components/ViewReport/ViewReport';
 
 function App() {
   return (
     <>
-    <GlobalFonts />
-    <Header/>
-    <BrowserRouter>
-        <Route path="/login" component={Login} exact />
-        <Route path="/UserProfile" component={UserProfile} exact />
-        <Route path="/MyProfile" component={MyProfile} exact />
+      <GlobalFonts />
+      <BrowserRouter>
+        <Route path="/" component={UserRouter}/>
+        <ViewReport/>
       </BrowserRouter>
     </>
   );
