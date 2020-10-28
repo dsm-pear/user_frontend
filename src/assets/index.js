a37b4ff7c98a651a7c9cbf254431b67c10ca0077
강은빈 <kub0803@naver.com>
2020-10-28 18:49:46 +0900

    [Modify] 이미지 index

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,4 @@
-export { default as MainBackground } from "./MainBackground.png"; 
-export { default as StudentProfile } from './StudentProfile.svg';
+export { default as MainBackground } from "./MainBackground.png";
+export { default as StudentProfile } from "./StudentProfile.svg";
+export { default as TeacherProfile } from './TeacherProfile.svg';
+export { default as PearLogo } from "./pearlogo.svg";
