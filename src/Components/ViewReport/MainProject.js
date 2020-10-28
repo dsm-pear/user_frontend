76d4cd83669efdcce64fdf7dad0ab612e1085e4e
강은빈 <kub0803@naver.com>
2020-10-28 23:36:17 +0900

    [Modify] 보고서 보기 수정

---
+++ src/Components/ViewReport/MainProject.js
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "../styled/ViewReport/style";
+
+function MainProject() {
+  return <S.MainProject />;
+}
+
+export default MainProject;
--- src/Components/ViewReport/ViewReport.js
+++ src/Components/ViewReport/ViewReport.js
@@ -1,11 +1,20 @@
 import React from "react";
-import * as R from "../styled/ViewReport/style";
+import * as S from "../styled/ViewReport/style";
+import ReportGrade from "./ReportGrade";
+import Categorybar from './Categorybar';
+import MainProject from './MainProject';
 
 function ViewReport() {
   return (
-    <R.Main>
-      <R.Cover></R.Cover>
-    </R.Main>
+    <S.Main>
+      <S.Cover>
+        <div className="coverCategory">
+          <ReportGrade grade="1" />
+          <Categorybar/>
+        </div>
+        <MainProject/>
+      </S.Cover>
+    </S.Main>
   );
 }
 
