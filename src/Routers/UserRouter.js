4ff2ed01b5f5c03c132489c3f6086561ab1eab65
강은빈 <kub0803@naver.com>
2020-11-03 11:36:53 +0900

    [Add] 카테고리 별로 라우터 추가

--- src/Routers/UserRouter.js
+++ src/Routers/UserRouter.js
@@ -6,6 +6,14 @@ import Login from "../Components/Login/Login";
 import UserProfile from "../Components/Profile/UserProfile";
 import MyProfile from "../Components/Profile/MyProfile";
 import ViewReport from '../Components/ViewReport/ViewReport';
+import ReportWeb from '../Components/ViewReport/ViewReportGroup/ReportWeb';
+import ReportApp from '../Components/ViewReport/ViewReportGroup/ReportApp';
+import ReportGame from '../Components/ViewReport/ViewReportGroup/ReportGame';
+import ReportAI from '../Components/ViewReport/ViewReportGroup/ReportAI';
+import ReportEmb from '../Components/ViewReport/ViewReportGroup/ReportEmb';
+import ReportInfo from '../Components/ViewReport/ViewReportGroup/ReportInfo';
+import ReportFus from '../Components/ViewReport/ViewReportGroup/ReportFus';
+
 
 function UserRouter() {
   return (
@@ -17,6 +25,17 @@ function UserRouter() {
         <Route path="/UserProfile" component={UserProfile} exact />
         <Route path="/MyProfile" component={MyProfile} exact />
         <Route path="/ViewReport" component={ViewReport} exact/>
+        {/* 보고서 파트별로  소프트웨어 개발과 */}
+        <Route path="/ViewReport/ReportWeb" component={ReportWeb} exact />
+        <Route path="/ViewReport/ReportApp" component={ReportApp} exact />
+        <Route path="/ViewReport/ReportGame" component={ReportGame} exact />
+        <Route path="/ViewReport/ReportAI" component={ReportAI} exact />
+        {/* 임베디드 개발과 */}
+        <Route path="/ViewReport/ReportEmb" component={ReportEmb} exact />
+        {/*정보 보안 */}
+        <Route path="/ViewReport/ReportInfo" component={ReportInfo} exact />
+        {/*융합 보고서 */}
+        <Route path="/ViewReport/ReportFus" component={ReportFus} exact />
       </Switch>
     </>
   );
