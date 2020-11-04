d0405a083bfef0f179edcffcabf433a892e64cd8
hanjunho <cloudtubekr@naver.com>
2020-11-04 23:26:27 +0900

    Header UI

--- src/Components/Main/Header.js
+++ src/Components/Main/Header.js
@@ -1,8 +1,60 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import Pearlogo from '../../img/pearlogo.svg';
+import Profile from '../../img/Profile.svg'
 import * as S from '../../style/MainStyled/HeaderStyle'
+import DownArrow from '../../img/DownArrow.png';
+import UpArrow from '../../img/UpArrow.png';
+import Seachimage from '../../img/Seachimage.png';
 
 const Header = () => {
+    const [ seachtype, setSeachtype ] = useState("제목");
+    const [ show, setShow ] = useState(false)
+    const [ report, setReport ] = useState(false)
+    const [ profile, setProfile ] = useState(false)
+    const [ img, setImg ] = useState(DownArrow)
+
+    const onlist = () => {
+        if(show === false){
+            setShow(true);
+            setImg(UpArrow);
+        }
+        else{
+            setShow(false);
+            setImg(DownArrow);
+        }
+        
+    }
+
+    const onChoice = (e) => {
+        if(e === 1){
+            setSeachtype("제목");
+        }
+        else if( e === 2 ){
+            setSeachtype("언어");
+        }
+        else if( e === 3 ){
+            setSeachtype("프로필");
+        }
+    }
+
+    const onReport = (e) => {
+        if(e){
+            setReport(true);
+        }
+        else{
+            setReport(false);
+        }
+    }
+
+    const onProfile = (e) => {
+        if(e){
+            setProfile(true);
+        }
+        else{
+            setProfile(false);
+        }
+    }
+
     return (
         <>
             <S.HeaderBox>
@@ -14,14 +66,22 @@ const Header = () => {
 
                     {/* 검색창 */}
                     <S.SeachBar>
-                        <S.SeachBarSelect name="china">
-                            <option value="1">제목</option>
-                            <option value="2">언어</option>
-                            <option value="3">게시물</option>
-                            <option value="4">프로필</option>
+                        <S.SeachBarSelect onClick={onlist}>
+                            <S.SeachChoice><img src={img} />{seachtype}</S.SeachChoice>
+                            { 
+                            show ? 
+                            <S.SeachList>
+                                <S.SeachType onClick={() => onChoice(1)}>제목</S.SeachType>
+                                <S.SeachType onClick={() => onChoice(2)}>언어</S.SeachType>
+                                <S.SeachType onClick={() => onChoice(3)}>프로필</S.SeachType>
+                            </S.SeachList>
+                            :null
+                            }
                         </S.SeachBarSelect>
+
                         <S.SeachBarInput placeholder="검색창"/>
-                        <S.SeachBarButton>검색</S.SeachBarButton>
+
+                        <S.SeachBarButton><img src={Seachimage}/></S.SeachBarButton>
                     </S.SeachBar>
 
                     {/* 메뉴 */}
@@ -29,7 +89,36 @@ const Header = () => {
                         <S.MenuUl>
                             <S.MenuList>공지사항</S.MenuList>
                             <S.MenuList>보고서 작성</S.MenuList>
-                            <S.MenuList>보고서 보기</S.MenuList>
+                            <S.MenuList onMouseEnter={() => onReport(true)} onMouseLeave={()=>onReport(false)}>
+                                보고서 보기
+                                {
+                                    report ?
+                                    <S.MenuSee>
+                                        <S.ReportSee>1학년</S.ReportSee>
+                                        <S.ReportSee>2학년</S.ReportSee>
+                                        <S.ReportSee>3학년</S.ReportSee>
+                                        <S.ReportSee>공통</S.ReportSee>
+                                    </S.MenuSee>
+                                : null
+                                }
+                                </S.MenuList>
+
+                            {     /*토큰 여부에 따라 출력*/
+                            <S.MenuList onMouseEnter={() => onProfile(true)} onMouseLeave={()=>onProfile(false)}>
+                                <S.Profile>프로필</S.Profile>
+                                {
+                                    profile ?
+                                    <S.Mypage>
+                                        <S.Mypro>MYPAGE</S.Mypro>
+                                        <S.Mypro>로그아웃</S.Mypro>
+                                    </S.Mypage>
+                                    :null
+                                }
+                                <S.Profile><img src={Profile} alt="Profile"/></S.Profile>
+                                
+                                
+                            </S.MenuList>
+                            }
                         </S.MenuUl>
                     </S.MenuBar>
                 </S.HeaderSubBox>
--- src/style/MainStyled/HeaderStyle.js
+++ src/style/MainStyled/HeaderStyle.js
@@ -2,11 +2,11 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const HeaderBox = styled.div`
-    margin-top: 51.5px;
-    display: flex;
-    margin-left: 177px;
+    padding-top: 40px;
+    display: fixed;
+    margin-left: 150px;
     margin-right: 222px;
-    
+    z-index: 10;
 `;
 
 export const HeaderSubBox = styled.div`
@@ -30,7 +30,7 @@ export const SeachBar = styled.div`
     
 `;
 
-export const SeachBarSelect = styled.select`
+export const SeachBarSelect = styled.div`
     font-size: 13px;
     width: 70px;
     border: none;
@@ -39,6 +39,37 @@ export const SeachBarSelect = styled.select`
     cursor: pointer;
     &:focus {
         outline: none;
+    }
+`;
+
+export const SeachChoice = styled.div`
+    cursor: pointer;
+    text-align: center;
+    padding-top: 11px;
+    img{
+        width: 10px;
+        margin-right: 3px;
+        color: #000000;
+    }
+`;
+
+export const SeachList = styled.div`
+    cursor: pointer;
+    margin-top: 20px;
+    z-index: 5;
+    width: 58px;
+    height: 95px;
+    border-radius: 6px;
+    background-color: #e4eef2;
+    padding-top: 12px;
+    padding-left: 10px;
+`;
+
+export const SeachType = styled.div`
+    font-size: 13px;
+    margin-bottom: 8px;
+    &:hover{
+        color : #5955d8;
     }
 `;
 
@@ -61,11 +92,14 @@ export const SeachBarButton = styled.button`
     &:focus {
         outline: none;
     }
+    img{
+        width: 20px;
+    }
 `;
 
 
 export const MenuBar = styled.div`
-    width: 475px;
+    width: 550px;
 `;
 
 export const MenuUl = styled.ul`
@@ -75,8 +109,65 @@ export const MenuUl = styled.ul`
 
 export const MenuList = styled.li `
     list-style: none;
+    align-items: center;
+    display: flex;
     cursor: pointer;
     &:hover{
-        color:#5955d8
+        color:#5955d8;
+    }
+    div{
+    }
+`;
+
+export const MenuSee = styled.div`
+    width: 85px;
+    height: 138px;
+    padding-top: 10px;
+    border: solid 1px #e4eef2;
+    background-color: #ffffff;
+    text-align: center;
+    cursor: pointer;
+    margin-top: 90px;
+    z-index: 5;
+    position: absolute;
+`;
+
+export const ReportSee = styled.div`
+    font-size: 15px;
+    margin-bottom: 14px;
+    color: #000000;
+    cursor: pointer;
+    &:hover{
+        font-size: 16px;
+        color:#5955d8;
+    }
+`;
+export const Profile = styled.div`
+    img{
+        margin-left: 20px;
+        width:31px;
+    }
+`;
+
+export const Mypage = styled.div`
+    width: 80px;
+    height: 73px;
+    padding-top: 18px;
+    border: solid 1px #e4eef2;
+    background-color: #ffffff;
+    text-align: center;
+    position: absolute;
+    margin-top: 65px;
+    margin-left: 5px;
+    z-index: 5;
+`;
+
+export const Mypro = styled.div`
+    font-size: 15px;
+    margin-bottom: 10px;
+    color: #000000;
+    &:hover{
+        font-size: 16px;
+        color:#5955d8;
     }
 `;
