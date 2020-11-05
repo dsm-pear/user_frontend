da7b3eb11c482507fb6ebce3d10f6a3b334ba9a7
강은빈 <kub0803@naver.com>
2020-11-05 23:30:39 +0900

    [Modify]  유저 라우터 수정

--- src/Routers/UserRouter.js
+++ src/Routers/UserRouter.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import GlobalFonts from "../GlobalStyle";
-import Header from "../Components/Main/Header";
 import Login from "../Components/Login/Login";
+import SignUp from '../Components/Login/SignUp';
 import UserProfile from "../Components/Profile/UserProfile";
 import MyProfile from "../Components/Profile/MyProfile";
 import ViewReport from '../Components/ViewReport/ViewReport';
@@ -19,9 +19,9 @@ function UserRouter() {
   return (
     <>
       <GlobalFonts />
-      <Header />
       <Switch>
         <Route path="/Login" component={Login} exact />
+        <Route path="/Signup" component={SignUp} exact/>
         <Route path="/UserProfile" component={UserProfile} exact />
         <Route path="/MyProfile" component={MyProfile} exact />
         <Route path="/ViewReport" component={ViewReport} exact/>
