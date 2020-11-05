854c8cbed2dbe25e93cf2eaebf90d66776ef47ac
hanjunho <cloudtubekr@naver.com>
2020-11-05 23:12:04 +0900

    searchImg

--- src/Components/Main/Header.js
+++ src/Components/Main/Header.js
@@ -4,7 +4,7 @@ import Profile from '../../assets/StudentProfile.svg'
 import * as S from '../../style/MainStyled/HeaderStyle'
 import DownArrow from '../../assets/ArrowImg/DownArrow.png';
 import UpArrow from '../../assets/ArrowImg/UpArrow.png';
-import Searchimage from '../../assets/SearchImg.png';
+import Searchimage from '../../assets/searchImg.png';
 
 const Header = () => {
     const [ searchtype, setSearchtype ] = useState("제목");
