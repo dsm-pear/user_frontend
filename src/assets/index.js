d376a3e321298e7e5b023ddbd1e7d405f4d0b3af
Hyeonyee <jgh10070@naver.com>
2020-11-06 19:24:17 +0900

    add logo & img link

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,6 @@
 export { default as MainBackground } from "./MainBackground.png"; 
 export { default as StudentProfile } from './StudentProfile.svg';
+export { default as RWlogo} from './RWlogo.svg';
+export { default as link} from './link.svg';
+export { default as searchImg} from './searchImg.png';
+export { default as select} from './select.svg';
