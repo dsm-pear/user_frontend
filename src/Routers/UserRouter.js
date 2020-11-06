e370206cc1b5e814e3092ec8ccceab7c490d2fdc
강은빈 <kub0803@naver.com>
2020-11-06 13:19:20 +0900

    [Modify]  라우터 수정

--- src/Routers/UserRouter.js
+++ src/Routers/UserRouter.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
 import GlobalFonts from "../GlobalStyle";
-import Login from "../Components/Login/Login";
-import SignUp from '../Components/Login/SignUp';
-import UserProfile from "../Components/Profile/UserProfile";
-import MyProfile from "../Components/Profile/MyProfile";
-import ViewReport from '../Components/ViewReport/ViewReport';
-import ReportWeb from '../Components/ViewReport/ViewReportGroup/ReportWeb';
-import ReportApp from '../Components/ViewReport/ViewReportGroup/ReportApp';
-import ReportGame from '../Components/ViewReport/ViewReportGroup/ReportGame';
-import ReportAI from '../Components/ViewReport/ViewReportGroup/ReportAI';
-import ReportEmb from '../Components/ViewReport/ViewReportGroup/ReportEmb';
-import ReportInfo from '../Components/ViewReport/ViewReportGroup/ReportInfo';
-import ReportFus from '../Components/ViewReport/ViewReportGroup/ReportFus';
-
+import {
+  Login,
+  SignUp,
+  UserProfile,
+  MyProfile,
+  ViewReport,
+  ReportAI,
+  ReportApp,
+  ReportEmb,
+  ReportFus,
+  ReportGame,
+  ReportInfo,
+  ReportWeb,
+} from "../Components/index";
 
 function UserRouter() {
   return (
@@ -21,10 +22,10 @@ function UserRouter() {
       <GlobalFonts />
       <Switch>
         <Route path="/Login" component={Login} exact />
-        <Route path="/Signup" component={SignUp} exact/>
+        <Route path="/Signup" component={SignUp} exact />
         <Route path="/UserProfile" component={UserProfile} exact />
         <Route path="/MyProfile" component={MyProfile} exact />
-        <Route path="/ViewReport" component={ViewReport} exact/>
+        <Route path="/ViewReport" component={ViewReport} exact />
         {/* 보고서 파트별로  소프트웨어 개발과 */}
         <Route path="/ViewReport/ReportWeb" component={ReportWeb} exact />
         <Route path="/ViewReport/ReportApp" component={ReportApp} exact />
