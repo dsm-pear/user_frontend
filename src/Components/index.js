a055591dd65d23127f26a7e4be65dafcc085fff8
hanjunho <cloudtubekr@naver.com>
2020-11-07 21:32:51 +0900

    Router Index

---
+++ src/Components/index.js
@@ -0,0 +1,15 @@
+export { default as Login } from "./Login/Login";
+export { default as SignUp } from "./Login/SignUp";
+export { default as UserProfile } from "./Profile/UserProfile";
+export { default as MyProfile } from "./Profile/MyProfile";
+export { default as ViewReport } from "./ViewReport/ViewReport";
+export { default as ReportWeb } from "./ViewReport/ViewReportGroup/ReportWeb";
+export { default as ReportApp } from "./ViewReport/ViewReportGroup/ReportApp";
+export { default as ReportGame } from "./ViewReport/ViewReportGroup/ReportGame";
+export { default as ReportAI } from "./ViewReport/ViewReportGroup/ReportAI";
+export { default as ReportEmb } from "./ViewReport/ViewReportGroup/ReportEmb";
+export { default as ReportInfo } from "./ViewReport/ViewReportGroup/ReportInfo";
+export { default as ReportFus } from "./ViewReport/ViewReportGroup/ReportFus";
+export { default as ReportWriting } from "./ReportWriting/ReportWriting";
+export { default as Notice } from "./Notice/Notice";
+export { default as NoticeContent } from "./Notice/NoticeContent";
