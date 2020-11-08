e2d36dc8e45eb56f500d03b3bc6128625bd478fd
Hyeonyee <jgh10070@naver.com>
2020-11-08 21:08:57 +0900

    add imgs

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,3 +4,5 @@ export { default as RWlogo} from './RWlogo.svg';
 export { default as link} from './link.svg';
 export { default as searchImg} from './searchImg.png';
 export { default as select} from './select.svg';
+export { default as Close} from './Close.svg';
+export { default as NowTeam} from './NowTeam.svg';
