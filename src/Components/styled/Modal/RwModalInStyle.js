398d7bab597eda5b037567f313651b2cd3cbec3c
Hyeonyee <jgh10070@naver.com>
2020-11-10 23:28:36 +0900

    add RwModalInStyle

--- src/Components/styled/Modal/RwModalInStyle.js
+++ src/Components/styled/Modal/RwModalInStyle.js
@@ -1,1 +1,30 @@
 import styled from "styled-components";
+
+const BorderInput = styled.div`
+    width: 100%;
+
+    form > span {
+        width: 100%;
+        display: flex;
+        justify-content: left;
+        align-items: center;
+    }
+    span > input {
+        width: 90%;    
+        border: none;
+        border-radius: 20px;
+        background-color: #ffffff;
+        outline: none;
+        padding: .5rem 1rem;
+        font-size: 15px;
+        font-family: BBTreeGR;
+    }
+    span > img {
+        width: 14px;
+    }
+`;
+
+export {
+    BorderInput,
+
+}
