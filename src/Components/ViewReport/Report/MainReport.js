3d2051685e3ae1c945a82d9a567746ae3bcbfe76
강은빈 <kub0803@naver.com>
2020-11-10 23:31:48 +0900

    [Modify] 보고서 보기 틀

--- src/Components/ViewReport/Report/MainReport.js
+++ src/Components/ViewReport/Report/MainReport.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import * as S from '../../styled/ViewReport/MainStyle';
+import React from "react";
+import * as S from "../../styled/ViewReport/MainStyle";
+import { createGlobalStyle } from "styled-components";
+import { MainScrollBackground } from "../../../assets/index";
+import ReportHeader from "./ReportHeader";
 
 function MainReport() {
+  const Globlastyle = createGlobalStyle`
+  body{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-image: url(${MainScrollBackground});
+    //background-size: 100vw 100vh;
+    background-repeat: no-repeat;
+  }`;
 
-    return(
-        <S.Main>
-            
-        </S.Main>
-
-    )
-    
+  return (
+    <>
+      <Globlastyle />
+      <S.MainBox>
+        <ReportHeader
+          team="개인"
+          grade="1학년"
+          title="이 페이지는 보고서 보기 페이지 입니다."
+        />
+      </S.MainBox>
+    </>
+  );
 }
 
 export default MainReport;
