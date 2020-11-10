2f20ca539f166ae22cecd9c4fe1d9b883b289f2a
강은빈 <kub0803@naver.com>
2020-11-10 22:39:34 +0900

    [Modify] 라우터 주소 수정

--- src/Routers/UserRouter.js
+++ src/Routers/UserRouter.js
@@ -22,23 +22,23 @@ function UserRouter() {
     <>
       <GlobalFonts />
       <Switch>
-        <Route path="/Login" component={Login} exact />
-        <Route path="/SignUp" component={SignUp} exact />
-        <Route path="/UserProfile" component={UserProfile} exact />
-        <Route path="/MyProfile" component={MyProfile} exact />
-        <Route path="/ViewReport" component={ViewReport} exact />
+        <Route path="/login" component={Login} exact />
+        <Route path="/sign-up" component={SignUp} exact />
+        <Route path="/user-profile" component={UserProfile} exact />
+        <Route path="/my-profile" component={MyProfile} exact />
+        <Route path="/view-report" component={ViewReport} exact />
         {/* 보고서 파트별로  소프트웨어 개발과 */}
-        <Route path="/ViewReport/ReportWeb" component={ReportWeb} exact />
-        <Route path="/ViewReport/ReportApp" component={ReportApp} exact />
-        <Route path="/ViewReport/ReportGame" component={ReportGame} exact />
-        <Route path="/ViewReport/ReportAI" component={ReportAI} exact />
+        <Route path="/view-report/report-web" component={ReportWeb} exact />
+        <Route path="/view-report/report-app" component={ReportApp} exact />
+        <Route path="/view-report/report-game" component={ReportGame} exact />
+        <Route path="/view-report/report-ai" component={ReportAI} exact />
         {/* 임베디드 개발과 */}
-        <Route path="/ViewReport/ReportEmb" component={ReportEmb} exact />
+        <Route path="/view-report/report-emb" component={ReportEmb} exact />
         {/*정보 보안 */}
-        <Route path="/ViewReport/ReportInfo" component={ReportInfo} exact />
+        <Route path="/view-report/report-info" component={ReportInfo} exact />
         {/*융합 보고서 */}
-        <Route path="/ViewReport/ReportFus" component={ReportFus} exact />
-        <Route path="/ViewReport/main-report" component={MainReport} exact />
+        <Route path="/view-report/report-fus" component={ReportFus} exact />
+        <Route path="/view-report/main-report" component={MainReport} exact />
       </Switch>
     </>
   );
