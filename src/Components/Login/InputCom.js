5f94f9974b142845a1f8158f4d881319f20a51c4
강은빈 <kub0803@naver.com>
2020-11-11 22:39:45 +0900

    [Modify] 피드백 수정 완료

--- src/Components/Login/InputCom.js
+++ src/Components/Login/InputCom.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import * as S from "../styled/Login/style";
 
-const Input = ({ placeholder, button, type, setData, Data }) => {
+const Input = ({ placeholder, button, type, setData, Data, background }) => {
   //버튼 색 변경
   const [bcolor, setBcolor] = useState("#e1e1e1");
   const handler = (e) => {
@@ -13,25 +13,30 @@ const Input = ({ placeholder, button, type, setData, Data }) => {
     //회원가입 창에서 인풋 입력값 보내줌
     setData(e.target.value);
   };
-
-  const Chick = (e) => {
-    if (Data !== "") {
-      alert("이메일 확인이 되었습니다.");
-      console.log("강은빈 멍청이");
+  /* 버튼 클릭시  */
+  const buttonclick = (e) => {
+    if (button === "인증번호 발신") {
+      const emailRule = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i; //이메일 정규식
+      if (!emailRule.test(Data)) {
+        console.log("이메일이 규칙과 다릅니다.");
+        alert("올바른 이메일을 입력해주세요");
+      }
+      else{
+        alert("이메일이 발신 되었습니다.");
+      }
+    }
+    else if( button === "인증번호 확인"){
+      console.log("인증 확인");
     }
   };
   return (
-    <S.CoverInput>
-      <input type={type} onChange={handler} placeholder={placeholder}></input>
-      {button ? (
-        <S.Button
-          onClick={Chick}
-          style={{ background: bcolor }}
-          className="Number"
-        >
+    <S.CoverInput background={background}>
+      <input type={type} onChange={handler} placeholder={placeholder} ></input>
+      {button && (
+        <S.Button onClick={buttonclick} background={bcolor} className="Number">
           {button}
         </S.Button>
-      ) : null}
+      )}
     </S.CoverInput>
   );
 };
