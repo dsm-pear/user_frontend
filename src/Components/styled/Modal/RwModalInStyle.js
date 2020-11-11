66d710b6a4c84ec1343916508992f7fa7e2446c0
Hyeonyee <jgh10070@naver.com>
2020-11-11 12:00:06 +0900

    add RwModalInStyle

--- src/Components/styled/Modal/RwModalInStyle.js
+++ src/Components/styled/Modal/RwModalInStyle.js
@@ -16,7 +16,7 @@ const BorderInput = styled.div`
         background-color: #ffffff;
         outline: none;
         padding: .5rem 1rem;
-        font-size: 15px;
+        font-size: 16px;
         font-family: BBTreeGR;
     }
     span > img {
@@ -24,7 +24,49 @@ const BorderInput = styled.div`
     }
 `;
 
+const BorderResult = styled.div`
+    width: 90%;
+    height: 12%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 0 0 .4rem .8rem;
+    border: 1px solid #ffffff;
+    border-radius: 7px;
+    background-color: #ffffff;
+
+    div {
+        font-size: 13px;
+        font-family: BBTreeGL;
+    }
+
+    form > div {
+        display: flex;
+        align-items: center;
+    }
+
+    div > span {
+        margin-right: .6rem;
+    }
+
+    div > input {
+        margin-left: .6rem;
+    }
+`;
+
+const BorderState = styled.div`
+    display: flex;
+    align-items: center;
+    font-size: 14px;
+    font-family: BBTreeGL;
+    font-weight: 600;
+        img {
+            margin-left: 1rem;
+        }
+`;
+
 export {
     BorderInput,
-
+    BorderResult,
+    BorderState
 }
