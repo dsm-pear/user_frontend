12028f1745c2ea0c6be01adc08d0ab3c9ba5748a
강은빈 <kub0803@naver.com>
2020-11-11 22:45:23 +0900

    [Add] 이미지 추가(강조)

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 export { default as MainBackground } from "./MainBackground.png"; 
 export { default as StudentProfile } from './StudentProfile.svg';
 export { default as RWlogo} from './RWlogo.svg';
@@ -6,3 +7,17 @@ export { default as searchImg} from './searchImg.png';
 export { default as select} from './select.svg';
 export { default as Close} from './Close.svg';
 export { default as NowTeam} from './NowTeam.svg';
+=======
+export { default as MainBackground } from "./MainBackground.png";
+
+export { default as StudentProfile } from "./StudentProfile.svg";
+export { default as TeacherProfile } from "./TeacherProfile.svg";
+export { default as PearLogo } from "./PEARlogo.svg";
+
+export { default as MainScrollBackground } from "./MainScrollBackground.svg";
+export { default as MainScrollBackground_1 } from "./MainScrollBackground-1.svg";
+export { default as MainScrollBackground_3 } from "./MainScrollBackground-3.svg";
+export { default as MainScrollBackground_4 } from "./MainScrollBackground-4.svg";
+export { default as MainSmallLogo } from "./MainSmallLogo.svg";
+export { default as Emphasis } from './Emphasis.svg';
+>>>>>>> Stashed changes
