0ff342ded3239e60a5bfec4289ae80dd131c4c3e
Hyeonyee <jgh10070@naver.com>
2020-11-12 23:39:27 +0900

    add export SubmitReportModal in index.js

--- src/Components/index.js
+++ src/Components/index.js
@@ -13,3 +13,4 @@ export { default as ReportFus } from "./ViewReport/ViewReportGroup/ReportFus";
 export { default as ReportWriting } from "./ReportWriting/ReportWriting";
 export { default as ReportWritingModal } from "./Modal/ReportWritingModal";
 export { default as TeamStateModal } from "./Modal/TeamStateModal";
+export { default as SubmitReportModal } from "./Modal/SubmitReportModal";
