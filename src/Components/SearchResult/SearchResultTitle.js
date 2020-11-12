4a5625368c97cf6c0db52bc112d7dac4a74e8ab7
hanjunho <cloudtubekr@naver.com>
2020-11-12 20:09:42 +0900

    [add] 검색결과 제목, 언어 컨테이너

---
+++ src/Components/SearchResult/SearchResultTitle.js
@@ -0,0 +1,92 @@
+import React, { useState } from 'react';
+import * as S from '../styled/SearchResult/SearchResultStyle';
+
+const SearchResultTitle = () => {
+    
+    const data = [
+        {
+            "id": 1,
+            "bdc": "개인",
+            "title": "제목",
+            "day": "2020.11.03"
+        },
+        {
+            "id": 2,
+            "bdc": "팀",
+            "title": "제목",
+            "day": "2020.11.03"
+        },
+        {
+            "id": 3,
+            "bdc": "동아리",
+            "title": "제목",
+            "day": "2020.11.03"
+        },
+        {
+            "id": 4,
+            "bdc": "개인",
+            "title": "제목",
+            "day": "2020.11.03"
+        },
+        {
+            "id": 5,
+            "bdc": "팀",
+            "title": "제목",
+            "day": "2020.11.03"
+        },
+        {
+            "id": 6,
+            "bdc": "동아리",
+            "title": "제목",
+            "day": "2020.11.03"
+        },
+        {
+            "id": 7,
+            "bdc": "개인",
+            "title": "제목",
+            "day": "2020.11.03"
+        },
+        {
+            "id": 8,
+            "bdc": "팀",
+            "title": "제목",
+            "day": "2020.11.03"
+        },
+        {
+            "id": 9,
+            "bdc": "동아리",
+            "title": "제목",
+            "day": "2020.11.03"
+        },
+    ]
+
+    
+
+    return (
+        <>
+            {
+                data.map((data)=>{
+                    const color = data.bdc === "팀" ? "#6192f3" : data.bdc === "개인" ? "#27d5b1" : "#5955d8";
+                    
+                    return(
+                        <S.Container bordercolor={color}>
+                            <S.ContainerContant>
+                                <S.ContainerBDC style={{color: color}}>
+                                    [{data.bdc}]
+                                </S.ContainerBDC>
+                                <S.ContainerTitle>
+                                    {data.title}
+                                </S.ContainerTitle>
+                                <S.ContainerDay>
+                                    {data.day}
+                                </S.ContainerDay>
+                            </S.ContainerContant>
+                        </S.Container>
+                    )
+                })
+            }
+        </>
+    )
+}
+
+export default SearchResultTitle;
