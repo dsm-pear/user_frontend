b0909c0d7e9bb182a92cdc19f0217b82029abf1b
hanjunho <cloudtubekr@naver.com>
2020-11-13 23:43:42 +0900

    [add] 검색결과 페이지

--- src/Components/SearchResult/SearchResult.js
+++ src/Components/SearchResult/SearchResult.js
@@ -2,11 +2,14 @@ import React, {useState} from 'react';
 import * as S from '../styled/SearchResult/SearchResultStyle';
 import SearchResultTitle from './SearchResultTitle';
 import SearchResultProfile from './SearchResultProfile';
+import SearchResultLanguage from './SearchResultLanguage';
 import Header from '../Main/Header';
 import DownArrow from '../../assets/ArrowImg/DownArrow.png'
 import UpArrow from '../../assets/ArrowImg/UpArrow.png'
 
-const SearchResult = () => {
+const SearchResult = (props) => {
+    const { params } = props.match;
+
     const [ range, setRange ] = useState("정렬");
     const [ show, setShow ] = useState(false);
     const [ img, setImg ] = useState(DownArrow);
@@ -30,6 +33,18 @@ const SearchResult = () => {
         setRange("오래된순");
     }
 
+    const asd = () => {
+        return(
+            params.data === "profile" ? 
+            <SearchResultProfile/>
+            : params.data === "title" ?
+            <SearchResultTitle/>
+            : <SearchResultLanguage/>
+        )
+    }
+
+    console.log(params);
+
     return(
         <>
         <S.Background>
@@ -51,9 +66,9 @@ const SearchResult = () => {
                     </S.ResultChoice>
 
                     <S.ResultContant>
-
-                        <SearchResultProfile/>
-                        <SearchResultTitle/>
+                        {
+                            asd()
+                        }
 
                     </S.ResultContant>
 
