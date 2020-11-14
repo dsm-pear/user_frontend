248f9515e75c9969a4a751b7831aae2efc2b0c9d
hanjunho <cloudtubekr@naver.com>
2020-11-14 23:38:10 +0900

    [add] Footer component, style

---
+++ src/Components/styled/MainStyled/FooterStyle.js
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+export const FooterBox = styled.div`
+    width: 100%;
+    height: 150px;
+    background-color: #f5fafc;
+    min-width: 1320px;
+`;
+
+export const FooterSubBox = styled.div`
+    width: 1500px;
+    height: 74px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    padding: 38px;
+`;
+
+export const Logo = styled.div`
+    margin-right: 15px;
+`;
+
+export const FooterContent = styled.div`
+    width: 800px;
+    font-size: 17px;
+    line-height: 1.28;
+    letter-spacing: 2.16px;
+    color: #000000;
+    margin-left: 30px;
+`;
+
+export const Develop = styled.div`
+    display: flex;
+`;
+
+export const Intro = styled.div`
+    width: 103px;
+    flex: 2;
+    margin-left: 60px;
+`;
+
+export const Title = styled.div`
+    text-align: center;
+    font-size: 17px;
+    line-height: 1.24;
+    letter-spacing: 0.34px;
+    margin-bottom: 8px;
+    font-weight: bold;
+`;
+
+export const People = styled.div`
+    height: 60px;
+    div{
+        display: inline;
+    }
+`;
