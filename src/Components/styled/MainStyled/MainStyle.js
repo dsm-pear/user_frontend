785155a1813547c7f65e384af21bd2552848964c
hanjunho <cloudtubekr@naver.com>
2020-11-16 13:23:01 +0900

    [modity] mainstyle

--- src/Components/styled/MainStyled/MainStyle.js
+++ src/Components/styled/MainStyled/MainStyle.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
-import main from '../../../assets/MainBackground.png';
+import main from '../../../assets/Main.png';
 
 export const MainBox = styled.div`
     background-image: url(${main});
-    background-size: 100% 100vh;
+    background-size: 100vw 100vh;
     height: 100vh;
-    width: 100%;
+    width: 100vw;
     background-repeat: no-repeat;
     min-width: 1520px;
 `;
