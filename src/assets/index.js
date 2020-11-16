f14d170d26d5eb00027b012e471655e9184a84ac
hanjunho <cloudtubekr@naver.com>
2020-11-17 08:32:50 +0900

    [modity] 이미지 인덱스

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,2 +1,7 @@
 export { default as MainBackground } from "./MainBackground.png"; 
 export { default as StudentProfile } from './StudentProfile.svg';
+export { default as RWlogo} from './RWlogo.svg';
+export { default as link} from './link.svg';
+export { default as searchImg} from './searchImg.png';
+export { default as select} from './select.svg';
+export { default as main} from './Main.png';
