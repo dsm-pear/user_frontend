803ec7bcd6e48c156926c5d68129eaef39b7c97e
Hyeonyee <jgh10070@naver.com>
2020-11-18 23:38:52 +0900

    add selecthover img path

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,5 +4,6 @@ export { default as RWlogo} from './RWlogo.svg';
 export { default as link} from './link.svg';
 export { default as searchImg} from './searchImg.png';
 export { default as select} from './select.svg';
+export { default as selecthover} from './SelectHover.svg';
 export { default as Close} from './Close.svg';
 export { default as NowTeam} from './NowTeam.svg';
