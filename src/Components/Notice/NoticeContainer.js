3c857f630a6e8dd5c322470052615bed72f54c17
hanjunho <cloudtubekr@naver.com>
2020-11-21 22:02:53 +0900

    [modity] notice

--- src/Components/Notice/NoticeContainer.js
+++ src/Components/Notice/NoticeContainer.js
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, {useCallback, useState} from 'react';
 import * as S from '../styled/NoticeStyled/NoticeStyle';
+import { Link } from 'react-router-dom';
 
-const NoticeContainer = () => {
+const NoticeContainer = (props) => {
+    
+
     const data = [
         {
             "id": 1,
@@ -15,7 +18,7 @@ const NoticeContainer = () => {
         },
         {
             "id": 3,
-            "title": "4번 제목",
+            "title": "3번 제목",
             "day": "2020.10.07"
         },
         {
@@ -53,26 +56,51 @@ const NoticeContainer = () => {
             "title": "10번 제목",
             "day": "2020.10.07"
         },
-        
     ]
+    const LimitData = props.limit;
+    const PageData = props.page;
+    const sort = props.sort;
+    const [ containerData, setContainerData ] = useState("");
+    const [ error, setError ] = useState(null);
+    const [ loading, setLoading ] = useState(null);
+
+    // axios.get(`https://api.dsm-pear.hs.kr/notice?size=${LimitData}&page=`})
+    //   .then(res => {
+    //     console.log(res);
+    //     if(res.status === 200){
+    //         setContentData(res.data)
+    //     }
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //     setError(error)
+    // });
+
+    const Container = useCallback(
+        (dataList)=>{
+            return dataList.map((data)=>{
+                return(
+                    <Link to={`/notice-content/${data.id}`} key={data.id}>
+                        <S.Container>
+                            <S.ContainerContant>
+                                <S.ContainerTitle>
+                                    {data.title}
+                                </S.ContainerTitle>
+                                <S.ContainerDay>
+                                    {data.day}
+                                </S.ContainerDay>
+                            </S.ContainerContant>
+                        </S.Container>
+                    </Link>
+                )
+            })
+        }
+    ,[]);
 
     return (
         <>
             {
-                data.map((data)=>{
-                    return(
-                    <S.Container key={data.id}>
-                        <S.ContainerContant>
-                            <S.ContainerTitle>
-                                {data.title}
-                            </S.ContainerTitle>
-                            <S.ContainerDay>
-                                {data.day}
-                            </S.ContainerDay>
-                        </S.ContainerContant>
-                    </S.Container>
-                    )
-                })
+                Container(data)
             }
         </>
     )
