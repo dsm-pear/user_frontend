ba2ed6a062da30b4b85660a52750c79c461c16e8
hanjunho <cloudtubekr@naver.com>
2020-11-21 13:15:10 +0900

    [modity] 오류수정

--- src/Components/SearchResult/SearchResultTitle.js
+++ src/Components/SearchResult/SearchResultTitle.js
@@ -1,5 +1,4 @@
-import React, { useState } from 'react';
-import { useCallback } from 'react';
+import React, { useCallback } from 'react';
 import * as S from '../styled/SearchResult/SearchResultStyle';
 
 const SearchResultTitle = () => {
