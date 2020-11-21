e94e5aafb77eb0f875f1867d717f479e89640667
hanjunho <cloudtubekr@naver.com>
2020-11-21 22:01:56 +0900

    [modity] notice style

--- src/Components/Notice/Notice.js
+++ src/Components/Notice/Notice.js
@@ -1,14 +1,23 @@
 import React, { useState } from 'react';
 import NoticeContainer from './NoticeContainer';
+import {Link} from 'react-router-dom';
 import Header  from '../Main/Header';
 import * as S from '../styled/NoticeStyled/NoticeStyle';
 import DownArrow from '../../assets/ArrowImg/DownArrow.png';
 import UpArrow from '../../assets/ArrowImg/UpArrow.png';
+import queryString from 'query-string';
 
-const Notice = () => {
+const Notice = ({location}) => {
     const [ range, setRange ] = useState("정렬");
     const [ show, setShow ] = useState(false);
     const [ img, setImg ] = useState(DownArrow);
+    const [ sort, setSort] = useState("#000000");
+
+    const query = queryString.parse(location.search);
+    const [ pageValue, setPageValue ] = useState(1);
+    let page_arr = [];
+    const page=5;
+    const limitdata = 7;
 
     const onChoice = () => {
         if(show){
@@ -23,12 +32,32 @@ const Notice = () => {
 
     const onNew = () => {
         setRange("최신순");
+        
     }
 
     const onOld = () => {
         setRange("오래된순");
     }
 
+    for(let i = 1; i <= page; i++) {
+        page_arr[i]=i;
+    }
+
+    const onSetPage = (e) => {
+        setPageValue(e)
+    }
+
+    const processed = () => page_arr.map((num)=>{
+        console.log(num);
+        if(query.page === num){
+            console.log(query.page)
+            return <Link onClick={()=>onSetPage(num)} to={`/notice?page=${page_arr[num]}`} color={sort} key={num}> {page_arr[num]} </Link>
+        }
+        else {
+            return <Link onClick={()=>onSetPage(num)} to={`/notice?page=${page_arr[num]}`} color={sort} key={num}> {page_arr[num]} </Link>
+        }
+    });
+    
     return(
         <>
         <S.Background>
@@ -40,26 +69,28 @@ const Notice = () => {
                     <S.NoticeChoice onClick={onChoice}>
                         <S.Noticearr>{range}<img src={img} alt="사진"/></S.Noticearr>
                         {
-                            show ?
+                            show &&
                             <S.NoticeRange>
                                 <S.NoticeC onClick={onNew}>최신순</S.NoticeC>
                                 <S.NoticeC onClick={onOld}>오래된순</S.NoticeC>
                             </S.NoticeRange>
-                            :null
                         }
                     </S.NoticeChoice>
 
                     <S.NoticeContant>
 
-                        <NoticeContainer/>
+                        <NoticeContainer limit={limitdata} page={pageValue} sort={sort}/>
 
                     </S.NoticeContant>
 
                     <S.NoticeAdd>
-                        더보기
-                        <S.NoticeAddImg>
-                            <img src={DownArrow} alt="사진"/>
-                        </S.NoticeAddImg>
+                        <S.NoticeAddNumber>
+
+                            {
+                                processed()
+                            }
+
+                        </S.NoticeAddNumber>
                     </S.NoticeAdd>
                     
                 </S.NoticeSubBox>
