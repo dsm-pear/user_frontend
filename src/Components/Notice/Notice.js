7f6537e087152798ad3ca349ee4cccfb827b75f1
hanjunho <cloudtubekr@naver.com>
2020-11-22 22:53:18 +0900

    page button

--- src/Components/Notice/Notice.js
+++ src/Components/Notice/Notice.js
@@ -11,9 +11,10 @@ const Notice = ({location}) => {
     const [ range, setRange ] = useState("정렬");
     const [ show, setShow ] = useState(false);
     const [ img, setImg ] = useState(DownArrow);
-    const [ sort, setSort] = useState("#000000");
+    const [ sort, setSort] = useState("");
 
     const query = queryString.parse(location.search);
+
     const [ pageValue, setPageValue ] = useState(1);
     let page_arr = [];
     const page=5;
@@ -32,7 +33,6 @@ const Notice = ({location}) => {
 
     const onNew = () => {
         setRange("최신순");
-        
     }
 
     const onOld = () => {
@@ -43,59 +43,53 @@ const Notice = ({location}) => {
         page_arr[i]=i;
     }
 
-    const onSetPage = (e) => {
-        setPageValue(e)
-    }
-
-    const processed = () => page_arr.map((num)=>{
-        console.log(num);
-        if(query.page === num){
-            console.log(query.page)
-            return <Link onClick={()=>onSetPage(num)} to={`/notice?page=${page_arr[num]}`} color={sort} key={num}> {page_arr[num]} </Link>
+    const processed = (querys) => page_arr.map((num)=>{
+        if(querys.page != num){
+            return <Link onClick={()=>setPageValue(num)} to={`/notice?page=${page_arr[num]}`} key={num}> {page_arr[num]} </Link>
         }
         else {
-            return <Link onClick={()=>onSetPage(num)} to={`/notice?page=${page_arr[num]}`} color={sort} key={num}> {page_arr[num]} </Link>
+            return <Link onClick={()=>setPageValue(num)} to={`/notice?page=${page_arr[num]}`} style={{color: "#6192f3"}} key={num}> {page_arr[num]} </Link>
         }
     });
-    
+
     return(
         <>
-        <S.Background>
-        <Header/>
-        
-            <S.NoticeBox>
-                <S.NoticeSubBox>
+            <S.Background>
+            <Header/>
+            
+                <S.NoticeBox>
+                    <S.NoticeSubBox>
 
-                    <S.NoticeChoice onClick={onChoice}>
-                        <S.Noticearr>{range}<img src={img} alt="사진"/></S.Noticearr>
-                        {
-                            show &&
-                            <S.NoticeRange>
-                                <S.NoticeC onClick={onNew}>최신순</S.NoticeC>
-                                <S.NoticeC onClick={onOld}>오래된순</S.NoticeC>
-                            </S.NoticeRange>
-                        }
-                    </S.NoticeChoice>
-
-                    <S.NoticeContant>
-
-                        <NoticeContainer limit={limitdata} page={pageValue} sort={sort}/>
-
-                    </S.NoticeContant>
-
-                    <S.NoticeAdd>
-                        <S.NoticeAddNumber>
-
+                        <S.NoticeChoice onClick={onChoice}>
+                            <S.Noticearr>{range}<img src={img} alt="사진"/></S.Noticearr>
                             {
-                                processed()
+                                show &&
+                                <S.NoticeRange>
+                                    <S.NoticeC onClick={onNew}>최신순</S.NoticeC>
+                                    <S.NoticeC onClick={onOld}>오래된순</S.NoticeC>
+                                </S.NoticeRange>
                             }
+                        </S.NoticeChoice>
 
-                        </S.NoticeAddNumber>
-                    </S.NoticeAdd>
-                    
-                </S.NoticeSubBox>
-            </S.NoticeBox>
-        </S.Background>
+                        <S.NoticeContant>
+
+                            <NoticeContainer limit={limitdata} page={pageValue} sort={sort}/>
+
+                        </S.NoticeContant>
+
+                        <S.NoticeAdd>
+                            <S.NoticeAddNumber>
+
+                                {
+                                    processed(query)
+                                }
+
+                            </S.NoticeAddNumber>
+                        </S.NoticeAdd>
+                        
+                    </S.NoticeSubBox>
+                </S.NoticeBox>
+            </S.Background>
         </>
     )
 }
