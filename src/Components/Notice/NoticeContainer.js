1bb62658cd9be61f798a3731d5eaf68ea510aa35
hanjunho <cloudtubekr@naver.com>
2020-11-23 20:10:44 +0900

    api 테스트

--- src/Components/Notice/NoticeContainer.js
+++ src/Components/Notice/NoticeContainer.js
@@ -1,80 +1,22 @@
-import React, {useCallback, useState} from 'react';
+import React, {useCallback, useState, useEffect} from 'react';
 import * as S from '../styled/NoticeStyled/NoticeStyle';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const NoticeContainer = (props) => {
-    
-
-    const data = [
-        {
-            "id": 1,
-            "title": "1번 제목",
-            "day": "2020.10.07"
-        },
-        {
-            "id": 2,
-            "title": "2번 제목",
-            "day": "2020.10.07"
-        },
-        {
-            "id": 3,
-            "title": "3번 제목",
-            "day": "2020.10.07"
-        },
-        {
-            "id": 4,
-            "title": "4번 제목",
-            "day": "2020.10.07"
-        },
-        {
-            "id": 5,
-            "title": "5번 제목",
-            "day": "2020.10.07"
-        },
-        {
-            "id": 6,
-            "title": "6번 제목",
-            "day": "2020.10.07"
-        },
-        {
-            "id": 7,
-            "title": "7번 제목",
-            "day": "2020.10.07"
-        },
-        {
-            "id": 8,
-            "title": "8번 제목",
-            "day": "2020.10.07"
-        },
-        {
-            "id": 9,
-            "title": "9번 제목",
-            "day": "2020.10.07"
-        },
-        {
-            "id": 10,
-            "title": "10번 제목",
-            "day": "2020.10.07"
-        },
-    ]
     const LimitData = props.limit;
     const PageData = props.page;
     const sort = props.sort;
     const [ containerData, setContainerData ] = useState("");
     const [ error, setError ] = useState(null);
     const [ loading, setLoading ] = useState(null);
-
-    // axios.get(`https://api.dsm-pear.hs.kr/notice?size=${LimitData}&page=`})
-    //   .then(res => {
-    //     console.log(res);
-    //     if(res.status === 200){
-    //         setContentData(res.data)
-    //     }
-    //   })
-    //   .catch(function (error) {
-    //     console.log(error);
-    //     setError(error)
-    // });
+    
+    const data = [
+        {
+            id: "1",
+            name: "ana",
+        }
+    ]
 
     const Container = useCallback(
         (dataList)=>{
@@ -84,10 +26,10 @@ const NoticeContainer = (props) => {
                         <S.Container>
                             <S.ContainerContant>
                                 <S.ContainerTitle>
-                                    {data.title}
+                                    {data.name}
                                 </S.ContainerTitle>
                                 <S.ContainerDay>
-                                    {data.day}
+                                    2020.11.23
                                 </S.ContainerDay>
                             </S.ContainerContant>
                         </S.Container>
@@ -97,10 +39,34 @@ const NoticeContainer = (props) => {
         }
     ,[]);
 
+    useEffect(()=>{
+        const DataApi = async () => {
+            try{
+                setError(null)
+                setContainerData(null);
+                setLoading(true);
+
+                const response = await axios.get(
+                    `https://jsonplaceholder.typicode.com/users`
+                );
+                setContainerData(response.data);
+            }catch(e){
+                setError(e);
+            }
+            setLoading(false);
+        };
+
+        DataApi();
+    }, []);
+
+    if (loading) return <div>로딩중..</div>;
+    if (error) return <div>{error}</div>;
+    if (!containerData) return null;
+
     return (
         <>
             {
-                Container(data)
+                Container(containerData)
             }
         </>
     )
