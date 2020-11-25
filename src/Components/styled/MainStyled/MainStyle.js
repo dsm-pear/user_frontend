1a1c44fccb6bc4761110c7bedfd4ee9d11b4ec22
hanjunho <cloudtubekr@naver.com>
2020-11-26 08:36:55 +0900

    mainstyle

---
+++ src/Components/styled/MainStyled/MainStyle.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import { main } from '../../../assets/index';
+
+export const MainBox = styled.div`
+    background-image: url(${main});
+    background-size: 100% 100%;
+    height: 100vh;
+    background-repeat: no-repeat;
+    min-width: 1320px;
+    min-height: 845px;
+    font-family: BBTreeGB;
+`;
