997da9ff17ce8e3b295bac92de066f5ae43eab42
강은빈 <kub0803@naver.com>
2020-12-17 09:01:58 +0900

    [Modify] 회원가입 피드백 수정

--- src/Components/Login/SignUp.js
+++ src/Components/Login/SignUp.js
@@ -15,7 +15,7 @@ const GlobalStyle = createGlobalStyle`
   }
 `;
 
-function SignUp() {
+function SignUp({ Data }) {
   const history = useHistory();
   //모든 창이 입력 되면 버튼색 바뀐
   const [button, setButton] = useState("#e1e1e1");
@@ -23,33 +23,18 @@ function SignUp() {
   /* 이름, 이메일, 이메일 확인, 비밀번호, 비밀번호 확인 */
   const [name, setName] = useState("");
   const [post, setPost] = useState("");
+  //인증번호 확인 
   const [check, setCheck] = useState("");
+
   const [password, setPassword] = useState("");
   const [pwconfirm, setPwconfirm] = useState("");
   //비밀번호 재 확인 색 변경
   const [pwInput, setPwInput] = useState("#e3f0ff");
   const [conInput, setConInput] = useState("#e3f0ff");
 
+
   //회원가입 API 연동
   const buttonCilckHandler = async () => {
-    try {
-      const { data } = await request(
-        "post",
-        "/account",
-        {},
-        {
-          name,
-          email: post,
-          password,
-        }
-      );
-      localStorage.setItem("token", data.token);
-      history.push("/login");
-    } catch (e) {
-      alert("이메일을 다시 확인해주세요");
-      console.log(e);
-    }
-
     //비밀번호 확인
     if (password.length < 8 || password.length >= 13) {
       alert("비밀번호를 8자 이상 13자 이하로 입력해주세요");
@@ -72,9 +57,41 @@ function SignUp() {
     } else if (password.length >= 8 && password.length <= 13) {
       /* 회원가입 조건 모두 만족 */
       if (password === pwconfirm) {
+        try {
+          const { data } = await request(
+            "post",
+            "/account",
+            {},
+            {
+              name,
+              email: post,
+              password,
+            }
+          );
+          localStorage.setItem("token", data.token);
+          history.push("/login");
+        } catch (e) {
+          alert("이메일을 다시 확인해주세요");
+          console.log(e);
+        }
         alert("회원가입에 성공하셨습니다.");
         history.push("/");
       }
+    }
+  };
+  //버튼 색 바뀌게 비교
+  const compare = (e) => {
+    //만약 이름이 널이면~
+    if (
+      name !== "" &&
+      post !== "" &&
+      check !== "" &&
+      password !== "" &&
+      pwconfirm !== ""
+    ) {
+      console.log("check");
+      setButton("#5955d8");
+      setBcolor("#ffffff");
     }
   };
 
@@ -105,7 +122,7 @@ function SignUp() {
     {
       type: "text",
       placeholder: "비밀번호를 입력해주세요",
-      setData: setPassword,
+      setData: setPassword,   
       data: null,
       button: null,
       background: pwInput,
@@ -120,51 +137,6 @@ function SignUp() {
       background: conInput,
     },
   ];
-
-  //버튼 색 바뀌게 비교
-  const compare = (e) => {
-    //만약 이름이 널이면~
-    if (
-      name !== "" &&
-      post !== "" &&
-      check !== "" &&
-      password !== "" &&
-      pwconfirm !== ""
-    ) {
-      console.log("check");
-      setButton("#5955d8");
-      setBcolor("#ffffff");
-    }
-  };
-
-  //비밀번호 재 확인
-  // const submit = (e) => {
-  //   /* 그냥 조건이 안맞음 */
-  //   if (password.length < 8 || password.length >= 13) {
-  //     alert("비밀번호를 8자 이상 13자 이하로 입력해주세요");
-  //     setPwInput("#ffeded");
-  //     setConInput("#e3f0ff");
-  //     /* 비밀번호의 값이 없으면 초기화 */
-  //     if (password === "") {
-  //       setPwInput("#e3f0ff");
-  //     }
-  //   } else if (password !== pwconfirm) {
-  //     /* 회원가입 조건중 비밀번호 확인 틀림 */
-  //     console.log("달라요");
-  //     setConInput("#ffeded");
-  //     setPwInput("#e3f0ff");
-  //     alert("비밀번호를 다시 확인해주세요");
-  //     if (pwconfirm === "") {
-  //       setConInput("#e3f0ff");
-  //     }
-  //   } else if (password.length >= 8 && password.length <= 13) {
-  //     /* 회원가입 조건 모두 만족 */
-  //     if (password === pwconfirm) {
-  //       alert("회원가입에 성공하셨습니다.");
-  //       history.push("/");
-  //     }
-  //   }
-  // };
 
   return (
     <>
