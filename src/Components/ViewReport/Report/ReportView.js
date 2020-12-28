778f377ceef3b4afe47e6016fac30f1d208a1f98
강은빈 <kub0803@naver.com>
2020-12-28 21:36:23 +0900

    [Modify] 보기 상세 내용

--- src/Components/ViewReport/Report/ReportView.js
+++ src/Components/ViewReport/Report/ReportView.js
@@ -2,23 +2,23 @@ import React from "react";
 
 import * as S from "../../styled/ViewReport/MainStyle";
 
-const ReportView = ({ title, text, git, file}) => {
+const ReportView = (props) => {
 
   return (
     <S.Contents>
       <S.Title>
-        <p>{title}</p>
+        <p>{props.title}</p>
       </S.Title>
       <S.MainText>
-        <p>{text}</p>
+        <p>{props.text}</p>
       </S.MainText>
       <S.Linkimg>
         <div className="git"></div>
-        <a href={git}>프로젝트 깃허브 구경하기</a>
+        <a href={props.git}>프로젝트 깃허브 구경하기</a>
       </S.Linkimg>
       <S.Linkimg>
         <div className="img"></div>
-        <p>{file}</p>
+        <p >{props.file}</p>
       </S.Linkimg>
     </S.Contents>
   );
