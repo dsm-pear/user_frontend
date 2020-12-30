f62ffa67a6576091b22557672ca4468fc13576ab
Hyeonyee <jgh10070@naver.com>
2020-12-30 23:17:49 +0900

    FIX ReportWritingModal

--- src/Components/Modal/ReportWritingModal.js
+++ src/Components/Modal/ReportWritingModal.js
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import * as S from '../styled/Modal/RwModalStyle';
-import * as I from '../styled/Modal/RwModalInStyle';
+import React, { useState } from "react";
+import * as S from "../styled/Modal/RwModalStyle";
+import * as I from "../styled/Modal/RwModalInStyle";
 import { Close } from "../../assets";
 import { searchImg } from "../../assets";
 import { NowTeam } from "../../assets";
@@ -8,70 +8,93 @@ import { clickNT } from "../../assets";
 import { checked } from "../../assets";
 import { bfchecked } from "../../assets";
 
-const ReportWritingModal = ({setOpen, setMyHei, open, myHei, opas}) => {
-    const [ toggled, setToggled ] = useState(false);
-    const [ toggle, setToggle ] = useState(false);
-    
-    const onClick = () => {
-        setOpen("hidden")
-        setMyHei("0")
-    }
+const ReportWritingModal = ({ setOpen, setMyHei, open, myHei, opas }) => {
+  const [toggled, setToggled] = useState(false);
+  const [toggle, setToggle] = useState(false);
 
-    const btnClick = () => {
-        setToggled(!toggled);
-    }
+  const onClick = () => {
+    setOpen("hidden");
+    setMyHei("0");
+  };
 
-    const clickCheckBox = () => {
-        setToggle(!toggle);
-    }
+  const btnClick = () => {
+    setToggled(!toggled);
+  };
 
+  const clickCheckBox = () => {
+    setToggle(!toggle);
+  };
 
-    return (
-        <S.Main visibility={open}>
-            <S.ModalMain height={myHei} opas={opas}>
-                <S.ModalSort>
-                    <S.CloseBtn onClick={onClick}>
-                        <span><img src={Close} alt="Close"/></span>
-                    </S.CloseBtn>
-                    <S.SearchInput>
-                        <I.BorderInput>
-                            <form name="input-name" action="" method="post">
-                                <span><input type={Text} /><img src={searchImg} alt="search" /></span>
-                            </form>
-                        </I.BorderInput>
-                    </S.SearchInput>
-                    <S.SearchResult>
-                        <I.BorderResult>
-                            <div>
-                                <form name="team-member" action="" method="post">
-                                    <I.BolderCheckBox>
-                                        <span>전규현(201215jgh@dsm.hs.kr)</span>
-                                        <div onClick={clickCheckBox}>
-                                        {
-                                            toggle === true ?
-                                            <img src={checked} alt="checked"/>
-                                            :<img src={bfchecked} alt="beforechecked"/>
-                                        }
-                                        </div>
-                                    </I.BolderCheckBox>
-                                </form>
-                           </div>
-                        </I.BorderResult>
-                    </S.SearchResult>
-                    <S.TeamState>
-                        <I.BorderState onClick={btnClick}>
-                            <span>현재 팀 상태</span>
-                            {
-                                toggled === true ?
-                                <img src={clickNT} alt="clickNT"/>
-                                :<img src={NowTeam} alt="NowTeam"/>
-                            }
-                        </I.BorderState>
-                    </S.TeamState>
-                </S.ModalSort>
-            </S.ModalMain>
-        </S.Main>
-    );
-}
+  return (
+    <S.Div visibility={open}>
+      <S.LeftModalMain height={myHei} opas={opas}>
+        <S.LeftModalSort>
+          <S.LeftCloseBtn onClick={onClick}>
+            <span>{toggled === !true && <img src={Close} alt="Close" />}</span>
+          </S.LeftCloseBtn>
+          <S.SearchInput>
+            <I.BorderInput>
+              <form name="input-name" action="" method="post">
+                <span>
+                  <input type={Text} />
+                  <img src={searchImg} alt="search" />
+                </span>
+              </form>
+            </I.BorderInput>
+          </S.SearchInput>
+          <S.SearchResult>
+            <I.BorderResult>
+              <div>
+                <form name="team-member" action="" method="post">
+                  <I.BolderCheckBox>
+                    <span>전규현(201215jgh@dsm.hs.kr)</span>
+                    <div onClick={clickCheckBox}>
+                      {toggle === true ? (
+                        <img src={checked} alt="checked" />
+                      ) : (
+                        <img src={bfchecked} alt="beforechecked" />
+                      )}
+                    </div>
+                  </I.BolderCheckBox>
+                </form>
+              </div>
+            </I.BorderResult>
+          </S.SearchResult>
+          <S.TeamState>
+            <I.BorderState onClick={btnClick}>
+              <span>현재 팀 상태</span>
+              {toggled === true ? (
+                <div>
+                  <img src={clickNT} alt="clickNT" />
+                </div>
+              ) : (
+                <img src={NowTeam} alt="NowTeam" />
+              )}
+            </I.BorderState>
+          </S.TeamState>
+        </S.LeftModalSort>
+      </S.LeftModalMain>
+      {toggled === true && (
+        <S.RightModalMain>
+          <S.RightModalSort>
+            <S.RightCloseBtn>
+              <span>
+                <img src={Close} alt="Close" />
+              </span>
+            </S.RightCloseBtn>
+            <S.ClickMember>
+              <I.MemberBox>
+                <div>
+                  <span>전규현(201215jgh@dsm.hs.kr)</span>
+                  <input type="checkbox" name="Teaminfo" value="member" />
+                </div>
+              </I.MemberBox>
+            </S.ClickMember>
+          </S.RightModalSort>
+        </S.RightModalMain>
+      )}
+    </S.Div>
+  );
+};
 
 export default ReportWritingModal;
