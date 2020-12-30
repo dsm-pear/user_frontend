b6c1732871e21584f8f75a485417c2a083c69718
강은빈 <kub0803@naver.com>
2020-12-30 23:24:06 +0900

    [Modify] 파일 서버 추가

--- src/utils/axios/axios.js
+++ src/utils/axios/axios.js
@@ -1,15 +1,35 @@
 import Axios from "axios";
-const URL = "http://20.55.121.118:8000";
+const MainURL = "http://10.156.147.50:8081";
+const FileURL = "http://10.156.147.50:3000";
+
 export function request(method, url, header, data) {
   return Axios({
     method,
-    url: URL + url,
+    url: MainURL + url,
     headers: header,
     data: data,
   })
     .then((res) => {
       console.log("다 받기 성공");
       console.log(res);
+      return res;
+    })
+    .catch((e) => {
+      console.error(e);
+    });
+}
+
+export function FileRequest(method, url, header, data) {
+  return Axios({
+    method,
+    url: FileURL + url,
+    headers: header,
+    data: data,
+  })
+    .then((res) => {
+      console.log("다 받기 성공");
+      console.log(res);
+      return res;
     })
     .catch((e) => {
       console.error(e);
