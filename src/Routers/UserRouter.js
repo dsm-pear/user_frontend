d0596dc61ba23785eeff1bab1e25486d47e11128
Hyeonyee <jgh10070@naver.com>
2021-01-11 01:44:39 +0900

    FIX UR

--- src/Routers/UserRouter.js
+++ src/Routers/UserRouter.js
@@ -12,9 +12,6 @@ import {
   Notice,
   NoticeContent,
   SearchResult,
-  ReportWritingModal,
-  SubmitReportModal,
-  SubmitSuccess,
   Main,
   ReportWeb,
 } from "../Components/index";
@@ -39,24 +36,6 @@ function UserRouter() {
 
         <Route path="/notice" component={Notice} exact />
         <Route path="/notice-content/:data" component={NoticeContent} exact />
-        <Route
-          path="/report-writing-modal"
-          component={ReportWritingModal}
-          exact
-        />
-
-        <Route
-          path="/report-writing-modal"
-          component={ReportWritingModal}
-          exact
-        />
-
-        <Route
-          path="/submit-report-modal"
-          component={SubmitReportModal}
-          exact
-        />
-        <Route path="/submit-succes-modal" component={SubmitSuccess} exact />
         {/*검색 조건에 따라 제목, 언어, 프로필*/}
 
         {/* 보고서 파트별로  소프트웨어 개발과 */}
