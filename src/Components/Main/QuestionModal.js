766515c1aaa30eaf97acba4529147e95b7631a45
Hyeonyee <jgh10070@naver.com>
2021-03-04 13:50:04 +0900

    add prettier

--- src/Components/Main/QuestionModal.js
+++ src/Components/Main/QuestionModal.js
@@ -1,68 +1,31 @@
-<<<<<<< HEAD
-import React from 'react';
-import * as S from '../styled/MainStyled/QuestionsStyle';
-import closeimg from '../../assets/closeimg.png';
+import React from "react";
+import * as S from "../styled/MainStyled/QuestionsStyle";
+import closeimg from "../../assets/closeimg.png";
 
-const QuestModal = ({
-    visible,
-    onClose,
-    message,
-    }) => {
-    
-      const close = (e) => {
-        if (onClose) {
-          onClose(e)
-        }
-      }
+const QuestModal = ({ visible, onClose, message }) => {
+  const close = (e) => {
+    if (onClose) {
+      onClose(e);
+    }
+  };
 
-    return (
-        <S.ModalBox visible={visible}>
-            <S.ModalBackground id="asd">
-                    <S.Modal>
-                        <S.ModalClose>
-                            <img src={closeimg} alt="사진" className="modal-close" onClick={close}/>
-                        </S.ModalClose>
-                        <S.ModalContant>
-                            {message}
-                        </S.ModalContant>
-                    </S.Modal>
-            </S.ModalBackground>
-        </S.ModalBox>
-    )
-}
+  return (
+    <S.ModalBox visible={visible}>
+      <S.ModalBackground id="asd">
+        <S.Modal>
+          <S.ModalClose>
+            <img
+              src={closeimg}
+              alt="사진"
+              className="modal-close"
+              onClick={close}
+            />
+          </S.ModalClose>
+          <S.ModalContant>{message}</S.ModalContant>
+        </S.Modal>
+      </S.ModalBackground>
+    </S.ModalBox>
+  );
+};
 
-=======
-import React from 'react';
-import * as S from '../styled/MainStyled/QuestionsStyle';
-import closeimg from '../../assets/closeimg.png';
-
-const QuestModal = ({
-    visible,
-    onClose,
-    message,
-    }) => {
-    
-      const close = (e) => {
-        if (onClose) {
-          onClose(e)
-        }
-      }
-
-    return (
-        <S.ModalBox visible={visible}>
-            <S.ModalBackground id="asd">
-                    <S.Modal>
-                        <S.ModalClose>
-                            <img src={closeimg} alt="사진" className="modal-close" onClick={close}/>
-                        </S.ModalClose>
-                        <S.ModalContant>
-                            {message}
-                        </S.ModalContant>
-                    </S.Modal>
-            </S.ModalBackground>
-        </S.ModalBox>
-    )
-}
-
->>>>>>> main
 export default QuestModal;
