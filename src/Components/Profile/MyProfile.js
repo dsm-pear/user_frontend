f25407364fd9bc01bfffbe4e3a8db5f75118aaa3
강은빈 <kub0803@naver.com>
2021-03-16 23:43:16 +0900

    [MODIFY] 마이프로필 API 수정

--- src/Components/Profile/MyProfile.js
+++ src/Components/Profile/MyProfile.js
@@ -1,68 +1,120 @@
 //내가 보는 내 프로필 수정 하기 누르기
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { request, useRefresh } from "../../utils/axios/axios";
 import * as S from "../styled/Profile/style";
-//import UserProfile from "./UserProfile";
+import Header from "../Main/Header";
 import Project from "./Project";
 import Profile from "./Profile";
 
-function MyProfile({}) {
+function MyProfile(props) {
   const [text, setText] = useState("수정");
+  const refreshHandler = useRefresh();
+
+  const [profileReport, setProfileReport] = useState([]);
+  const [profileData, setProfileData] = useState("");
+/*   const [userName, setUserName] = useState(null);
+  const [userEmail, setUserEmail] = useState(null);
+  const [introduce, setIntroduce] = useState(null); */
+
   //수정 누르면 저장으로 바뀌고 input disabled 가 해제됨
-  const ProModify = () => {
-    if (text === "저장") {
-      setText("수정");
-    } else {
+  //프로필 수정 API
+  const ChangeProfile = async () => {
+    if (text === "수정") {
       setText("저장");
+    } else {
+      try {
+        await request(
+          "put",
+          "/user/profile",
+          { Authorization: `Bearer ${localStorage.getItem("access-token")}` },
+          {
+            git_hub: props.github,
+            self_intro: props.introduce,
+          }
+        );
+      } catch (e) {
+        console.error(e);
+      }
+      setText("수정");
+      alert("프로필이 변경되었습니다.");
     }
   };
 
+  const getProfile = async () => {
+    try {
+      const { data } = await request(
+        "get",
+        "/user/profile",
+        { Authorization: `Bearer ${localStorage.getItem("access-token")}` },
+        ""
+      );
+
+      setProfileData(data);
+    } catch (e) {
+      //토큰 만료
+      console.error(e);
+    }
+  };
+
+  const getMyProject = async () => {
+    try {
+      const { data } = await request(
+        "get",
+        "/user/profile/report?size=6&page=0",
+        { Authorization: `Bearer ${localStorage.getItem("access-token")}` },
+        ""
+      );
+
+      console.log(data.myPageReportResponses.title);
+      setProfileReport(data.myPageReportResponses);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    getProfile();
+    getMyProject();
+  }, []);
+
   return (
     <S.Main>
+      <Header></Header>
       <S.MainProfile>
         {/* 좌측 프로필 */}
         <S.Cover>
           <Profile
-            name="강은빈"
-            email="kub9722@gmaeil.com"
-            produce="안녕하세요"
-            github="https://github.com/silverbeen"
+            name={profileData.userName}
+            email={profileData.userEmail}
+            produce={profileData.self_intro}
+            setProduce={profileData.setProduce}
+            github={profileData.git_hub}
+            //setGithub={setGithub}
             text={text}
           />
 
           {/* 프로젝트 보여주는 곳 */}
           <S.Project>
             <S.PreProject>
-              <Project team="개인" title="1301 강은빈입니다" date="20.10.26" />
-              <Project team="팀" title="1301 강은빈입니다" date="20.10.26" />
-              <Project
-                team="팀"
-                title="1301 강은빈입니다"
-                date="20.10.26"
-                save="[임시저장]"
-              />
-              <Project
-                team="개인"
-                title="1301 강은빈입니다"
-                date="20.10.26"
-                save ="[요청 중]"
-              />
-              <Project
-                team="동아리"
-                title="1301 강은빈입니다"
-                date="20.10.26"
-              />
-              <Project
-                team="동아리"
-                title="1301 강은빈입니다"
-                date="20.10.26"
-              />
+              {profileReport.map(() => (
+                <Project
+                  team={profileReport.team}
+                  title={profileReport.title}
+                  date={profileReport.createdAt}
+                />
+              ))}
               {/* 밑에 더보기 / 숫자 */}
-              <S.ProNum></S.ProNum>
+              {/* <M.Number>
+                <Link>1</Link>
+                <Link>2</Link>
+                <Link>3</Link>
+                <Link>4</Link>
+                <Link>5</Link>
+              </M.Number> */}
             </S.PreProject>
           </S.Project>
         </S.Cover>
-        <S.Modify onClick={ProModify}>{text}</S.Modify>
+        <S.Modify onClick={ChangeProfile}>{text}</S.Modify>
       </S.MainProfile>
     </S.Main>
   );
