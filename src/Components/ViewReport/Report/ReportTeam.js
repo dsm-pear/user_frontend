e64bcb6d62871ef21facb69c25f5affe5e6e6744
강은빈 <kub0803@naver.com>
2021-03-25 20:12:01 +0900

    [MODIFY] 보고서 팀원  추가

--- src/Components/ViewReport/Report/ReportTeam.js
+++ src/Components/ViewReport/Report/ReportTeam.js
@@ -1,24 +1,18 @@
 import React from "react";
-import SubMember from "./SubMember";
 import * as S from "../../styled/ViewReport/MainStyle";
 
 const ReportTeam = (props) => {
-  const TeamMember = [
-    {
-      member: "강은빈",
-    },
-    {
-      member: "강은빈",
-    },
-  ];
+  const member = props.members;
 
   return (
     <S.Teambox>
       <h2>{props.teamName}</h2>
       <S.TableCover>
-        {/* {TeamMember.map(({ member, i }) => (
-          <SubMember key={i} language={member} />
-        ))} */}
+        {member.map(({ memberName, index }) => (
+          <S.Member>
+            <span key={index}>{memberName}</span>
+          </S.Member>
+        ))}
       </S.TableCover>
     </S.Teambox>
   );
