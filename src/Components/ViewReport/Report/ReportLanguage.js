84340a2faa2a5b522892d5eaca355ef95e6ba8a1
강은빈 <kub0803@naver.com>
2021-03-25 20:12:13 +0900

    [MODIFY] 보고서 언어 추가

--- src/Components/ViewReport/Report/ReportLanguage.js
+++ src/Components/ViewReport/Report/ReportLanguage.js
@@ -1,38 +1,16 @@
 import React from "react";
 import * as S from "../../styled/ViewReport/MainStyle";
 
-const SubLanguage = ({ languages }) => {
-  return (
-    <S.Language>
-      <span>{languages}</span>
-    </S.Language>
-  );
-};
-
 const ReportLanguage = (props) => {
-  //const language = props.languages;
-  const LangCount = [
-    {
-      id: 1,
-      language: "JAVA",
-    },
-    {
-      id: 2,
-      language: "JAVA",
-    },
-    {
-      id: 2,
-      language: "Jsfsjfhlskhfsfhkjlsh",
-    },
-  ];
+  const language = props.languages.split(",");
 
   return (
     <S.CoverLang>
-      <h2>개발에 사용한 언어</h2>
+      <h2>사용된 언어</h2>
       <S.TableCover>
-        {LangCount.map((language) => (
-          <S.Language key={language.id}>
-            <span>{language.language}</span>
+        {language.map((language, index) => (
+          <S.Language key={index}>
+            <span>{language}</span>
           </S.Language>
         ))}
       </S.TableCover>
