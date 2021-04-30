7d603489a2d63f70023188ee2cc311df19883a5a
강은빈 <kub0803@naver.com>
2021-04-30 18:47:40 +0900

    [FIX] 프로필 페이지 완성

--- src/Components/Profile/UserProfile.js
+++ src/Components/Profile/UserProfile.js
@@ -1,65 +1,61 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { request, useRefresh } from "../../utils/axios/axios.js";
+import { request /* useRefresh */ } from "../../utils/axios/axios.js";
 import * as S from "../styled/Profile/style.js";
-import * as M from "../styled/ViewReport/style";
 import Profile from "./Profile";
-import Project from "./Project";
 import Header from "../Main/Header";
+import MainProject from "../../Components/Profile/MainProject";
+import { BoxLoading } from "react-loadingg";
 
 function UserProfile({ match }) {
   //내 프로젝트 리트
-  const [MyReportListResponses, setMyReportListResponses] = useState([]);
-  const [userProfile, setUserProfile] = useState([]);
-  const [github, setGithub] = useState("");
-  const [produce, setProduce] = useState("");
-  const refreshHandler = useRefresh();
+  const [myReportListResponses, setMyReportListResponses] = useState([]);
+  const [userProfile, setUserProfile] = useState("");
+  //const refreshHandler = useRefresh();
+  const [loding, setLoding] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     //유저 프로필 API
+
     const getProfile = async () => {
       try {
         const data = await request(
           "get",
-          `/profile?user-email=dummyemail@dsm.hs.kr`,
+          `/profile?user-email=${match.params.userEmail}`,
           { Authorization: `Bearer ${localStorage.getItem("access-token")}` },
           ""
         );
-        setUserProfile(data.userProfile);
+        setUserProfile(data.data);
       } catch (e) {
         console.error(e);
-        switch(e.data.status){
-          case 400:
-            alert("프로필 불러오기를 실패했습니다.");
-            break;
-          case 401:
-            refreshHandler().then(()=>{
-              getProfile();
-            })
-            break;
-          default:
-            break;
-        }
       }
+      setLoding(false);
+      setError(null);
     };
     //유저 프로젝트 API
     const getProject = async () => {
       try {
         const data = await request(
           "get",
-          `/profile/report?user-email=dummyemail@dsm.hs.kr&size=6&page=0`,
-          "",
+          `/profile/report?user-email=${match.params.userEmail}&size=&page=`,
+          { Authorization: `Bearer ${localStorage.getItem("access-token")}` },
           ""
         );
-
         setMyReportListResponses(data.data.profileReportResponses);
       } catch (e) {
         console.error(e);
       }
+      setLoding(false);
+      setError(null);
     };
+
     getProfile();
     getProject();
-  }, []);
+  }, [match.params.userEmail]);
+
+  if (loding) return <div>로딩중</div>;
+  if (error) return <div>에러입니다</div>;
+  if (!userProfile) return <BoxLoading />;
 
   return (
     <>
@@ -67,28 +63,27 @@ function UserProfile({ match }) {
         <Header></Header>
         <S.MainProfile>
           <S.Cover>
-            {<Profile
-              name={userProfile.userName}
-              email={userProfile.userEmail}
-              produce={userProfile.selfIntro}
-              setProduce={setProduce}
-              github={userProfile.gitHub}
-              setGithub={setGithub}
-            />}
+            {
+              <Profile
+                name={userProfile.userName}
+                email={userProfile.userEmail}
+                selfIntro={userProfile.selfIntro}
+                gitHub={userProfile.gitHub}
+              />
+            }
             {/* 프로젝트 보여주는 곳 */}
             <S.Project>
-              {MyReportListResponses.map(({ type, title, createdAt }) => (
-                <S.PreProject>
-                  <Project team={type} title={title} date={createdAt} />
-                </S.PreProject>
-              ))}
-            {/*   <M.Number>
-                <Link>1</Link>
-                <Link>2</Link>
-                <Link>3</Link>
-                <Link>4</Link>
-                <Link>5</Link>
-              </M.Number> */}
+              <S.PreProject>
+                {myReportListResponses.map((myReportListResponses, index) => (
+                  <MainProject
+                    key={index}
+                    reportId={myReportListResponses.reportId}
+                    type={myReportListResponses.type}
+                    title={myReportListResponses.title}
+                    date={myReportListResponses.createdAt.split("T")[0]}
+                  />
+                ))}
+              </S.PreProject>
             </S.Project>
           </S.Cover>
         </S.MainProfile>
