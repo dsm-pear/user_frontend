c8292407ed4647900a78f5a8ed9fcb4bdddb1288
oajh04 <cloudtubekr@naver.com>
2021-04-30 17:07:50 +0900

    UPDATE ; change Link

--- src/Components/SearchResult/SearchResultProfile.js
+++ src/Components/SearchResult/SearchResultProfile.js
@@ -1,29 +1,18 @@
-<<<<<<< HEAD
-import * as S from "../styled/SearchResult/SearchResultStyle";
-import { StudentProfile } from "../../assets";
-import React, { useCallback } from "react";
-=======
 import * as S from "../styled/SearchResult/SearchResultStyle.js";
 import { StudentProfile } from "../../assets";
 import React from "react";
->>>>>>> develop
 import { useHistory } from "react-router-dom";
 
 const SearchPropfile = ({ dataList }) => {
   const history = useHistory();
 
   const onLink = (email) => {
-<<<<<<< HEAD
     history.push({
       pathname: `/user-profile`,
       state: { useremail : email}
     });
   };
-  
-=======
-    history.push(`/user-profile/${email}`);
-  };
->>>>>>> develop
+
   return dataList.map((data) => (
     <div onClick={() => onLink(data.email)} key={data.email}>
       <S.Container style={{ color: "#6192f3" }}>
@@ -43,19 +32,8 @@ const SearchPropfile = ({ dataList }) => {
 const SearchResultProfile = (props) => {
   const searchData = props.data;
 
-<<<<<<< HEAD
   return <>{searchData ? <SearchPropfile dataList={searchData.userResponses} /> : <S.NoneData></S.NoneData>}</>;
-=======
-  return (
-    <>
-      {searchData ? (
-        <SearchPropfile dataList={searchData.userResponses} />
-      ) : (
-        <S.NoneData></S.NoneData>
-      )}
-    </>
-  );
->>>>>>> develop
+
 };
 
 export default SearchResultProfile;
