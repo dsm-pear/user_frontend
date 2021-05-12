a48af305a9fef2c738c8dcbdac5462c9e1b0fb03
Hyeonyee <jgh10070@naver.com>
2021-05-12 20:17:45 +0900

    [FIX] TeamSubmitReportModal

--- src/Components/ReportWriting/Modal/SubmitRequest/TeamReportRequest/TeamSubmitReportModal.jsx
+++ src/Components/ReportWriting/Modal/SubmitRequest/TeamReportRequest/TeamSubmitReportModal.jsx
@@ -123,7 +123,6 @@ const TeamSubmitReportModal = ({
           localStorage.removeItem("refresh-token");
           history.push("/");
         }
-        alert("제출 실패");
       });
   };
 
@@ -144,8 +143,8 @@ const TeamSubmitReportModal = ({
             </S.ModalHeader>
             <S.ModalMainText>
               <span>
-                첨부 파일의 확장자가 PDF형식이 맞는지 파일명은 프로젝트명 / 학번
-                / 이름
+                첨부 파일의 확장자가 PDF, 한글 형식이 맞는지 파일명은 프로젝트명
+                / 학번 / 이름
               </span>
               <p></p>
               <span>
