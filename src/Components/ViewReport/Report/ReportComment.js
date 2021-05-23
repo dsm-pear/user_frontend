e564ce94f01371041c69e933ffdefe22c13902e2
강은빈 <kub0803@naver.com>
2021-05-23 18:14:13 +0900

    [MODIFY] 스타일 & 피드백 수정

--- src/Components/ViewReport/Report/ReportComment.js
+++ src/Components/ViewReport/Report/ReportComment.js
@@ -1,17 +1,13 @@
 import React, { useState } from "react";
-import { Link, useHistory } from "react-router-dom";
-import { request /* useRefresh */ } from "../../../utils/axios/axios";
+import { Link } from "react-router-dom";
+import { request } from "../../../utils/axios/axios";
 import * as S from "../../styled/ViewReport/MainStyle";
 
 const ReportComment = (props) => {
   const [value, setValue] = useState("");
-  const history = useHistory();
-  //코멘트 버튼 클릭시
 
   const comments = props.comments;
   const reportId = props.reportId;
-
-  //const refreshHandler = useRefresh();
 
   const onChange = (e) => {
     setValue(e.target.value);
@@ -37,7 +33,6 @@ const ReportComment = (props) => {
   };
 
   //댓글 삭제
-  //코멘트 아이디 가져오기
   const deleteReportComment = async (commentId) => {
     console.log(commentId);
     try {
