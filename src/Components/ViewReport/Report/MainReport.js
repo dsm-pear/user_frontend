2f2ad1db32c1443cc5325c7ea2a0f7e8ea0878bd
강은빈 <kub0803@naver.com>
2021-05-24 22:49:12 +0900

    [FIX] 파일서버 끝

--- src/Components/ViewReport/Report/MainReport.js
+++ src/Components/ViewReport/Report/MainReport.js
@@ -17,6 +17,8 @@ function MainReport({ match }) {
   const [loding, setLoding] = useState(null);
   const [error, setError] = useState(null);
 
+  console.log(match.params.reportId);
+
   //토큰 검사
   const refreshHandler = useRefresh();
 
@@ -77,7 +79,17 @@ function MainReport({ match }) {
           text={reportData.description}
           git={reportData.github}
           file={reportData.fileName}
-          fileId="3"
+  
+          reportId={match.params.reportId}
+          languages={languages}
+          team={reportData.type}
+          access={reportData.access}
+          field={reportData.field}
+          grade={reportData.grade}
+          isSubmitted={reportData.isSubmitted}
+          teamName={reportData.teamName}
+          members={members}
+          fileId={reportData.fileId}
         />
         {reportData.type === "SOLE" ? null : (
           <ReportTeam teamName={reportData.teamName} members={members} />
--- src/Components/ViewReport/Report/ReportView.js
+++ src/Components/ViewReport/Report/ReportView.js
@@ -1,19 +1,200 @@
 import React from "react";
-import { FileURL } from "../../../utils/axios/axios";
+import { useHistory, useLocation } from "react-router";
+import { FileURL, MainURL } from "../../../utils/axios/axios";
 import * as S from "../../styled/ViewReport/MainStyle";
+import axios from "axios";
 
 const ReportView = (props) => {
   const fileId = props.fileId;
   const fileDownloadHandler = () => {
-    window.open(FileURL + `/report/${fileId}`);
+    window.open(
+      FileURL +
+        `/report/${fileId}?token=Bearer ${localStorage.getItem("access-token")}`
+    );
+  };
+
+  const history = useHistory();
+  const reportId = props.reportId;
+
+  console.log(props.isSubmitted);
+
+  const isModifyReport = () => {
+    switch (props.team) {
+      case "SOLE":
+        axios
+          .patch(
+            `${MainURL}/report/sole/${reportId}`,
+            {
+              title: `${props.title}`,
+              description: `${props.text}`,
+              languages: props.languages,
+              type: `${props.team}`,
+              access: `${props.access}`,
+              field: `${props.field}`,
+              grade: `${props.grade}`,
+              isSubmitted: props.isSubmitted,
+              github: `${props.git}`,
+            },
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("access-token")}`,
+              },
+            }
+          )
+          .then((res) => {
+            console.log(res);
+            axios.put(`/report/${fileId}`, props.file, {
+              headers: {
+                "Content-Type": "multipart/form-data", // multipart = 파일 업로드
+                Authorization: `Bearer ${localStorage.getItem("access-token")}`,
+              },
+            });
+          })
+          .catch((err) => {
+            if (err.response.status === 403) {
+              alert("권한이 없습니다.");
+
+              localStorage.removeItem("access-token");
+              localStorage.removeItem("refresh-token");
+              history.push("/");
+            } else if (err.response.status === 401) {
+            }
+          });
+        break;
+      case "TEAM":
+        axios
+          .patch(
+            `${MainURL}/report/team/${reportId}`,
+            {
+              title: `${props.title}`,
+              description: `${props.text}`,
+              languages: props.languages,
+              type: `${props.team}`,
+              access: `${props.access}`,
+              field: `${props.field}`,
+              grade: `${props.grade}`,
+              isSubmitted: false,
+              fileName: `${props.fileName}`,
+              github: `${props.git}`,
+              teamName: `${props.teamName}`,
+              members: props.members,
+            },
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("access-token")}`,
+              },
+            }
+          )
+          .then((res) => {
+            const id = res.id;
+            axios.put(`${FileURL}/report/${id}`, props.file, {
+              headers: {
+                "Content-Type": "multipart/form-data", // multipart = 파일 업로드
+                Authorization: `Bearer ${localStorage.getItem("access-token")}`,
+              },
+            });
+          })
+          .catch((err) => {
+            if (err.response.status === 403) {
+              alert("권한이 없습니다.");
+
+              localStorage.removeItem("access-token");
+              localStorage.removeItem("refresh-token");
+              history.push("/");
+            }
+          });
+        break;
+      case "CIRCLE":
+        axios
+          .patch(
+            `${MainURL}/report/team/${reportId}`,
+            {
+              title: `${props.title}`,
+              description: `${props.text}`,
+              languages: props.languages,
+              type: `${props.team}`,
+              access: `${props.access}`,
+              field: `${props.field}`,
+              grade: `${props.grade}`,
+              isSubmitted: false,
+              fileName: `${props.fileName}`,
+              github: `${props.git}`,
+              teamName: `${props.teamName}`,
+              members: props.members,
+            },
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${localStorage.getItem("access-token")}`,
+              },
+            }
+          )
+          .then((res) => {
+            const id = res.id;
+            axios.put(`${FileURL}/report/${id}`, props.file, {
+              headers: {
+                "Content-Type": "multipart/form-data", // multipart = 파일 업로드
+                Authorization: `Bearer ${localStorage.getItem("access-token")}`,
+              },
+            });
+          })
+          .catch((err) => {
+            if (err.response.status === 403) {
+              alert("권한이 없습니다.");
+
+              localStorage.removeItem("access-token");
+              localStorage.removeItem("refresh-token");
+              history.push("/");
+            }
+          });
+        break;
+      default:
+        console.log("err");
+    }
+  };
+
+  const isDeleteReprot = () => {
+    axios
+      .delete(
+        `${MainURL}/report/${reportId}`,
+        {
+          id: `${reportId}`,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("access-token")}`,
+          },
+        }
+      )
+      .then((res) => {
+        const id = res.id;
+        axios.delete(`${FileURL}/report/${id}`, props.file, {
+          headers: {
+            "Content-Type": "multipart/form-data", // multipart = 파일 업로드
+            Authorization: `Bearer ${localStorage.getItem("access-token")}`,
+          },
+        });
+      })
+      .catch((err) => {
+        if (err.response.status === 403) {
+          alert("권한이 없습니다.");
+
+          localStorage.removeItem("access-token");
+          localStorage.removeItem("refresh-token");
+          history.push("/");
+        }
+      });
   };
 
   return (
     <S.Contents>
       {/* 보고서 수정 삭제   */}
       <S.Modify>
-        <button>수정</button>
-        <button>삭제</button>
+        <button onClick={isModifyReport}>수정</button>
+        <button onClick={isDeleteReprot}>삭제</button>
       </S.Modify>
       <S.Title>
         <p>{props.title}</p>
@@ -31,7 +212,7 @@ const ReportView = (props) => {
       </S.Linkimg>
       <S.Linkimg>
         <div className="img"></div>
-        {props.file === null ? (
+        {props.fileId === null ? (
           <div style={{ color: "gray", marginLeft: "5px" }}>
             파일이 존재하지 않습니다.
           </div>
