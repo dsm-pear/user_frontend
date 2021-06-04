578da3b391ca4602c03fae963770f0a56f7c65b4
Hyeonyee <jgh10070@naver.com>
2021-06-04 18:43:48 +0900

    [FIX] Report

--- src/Components/ViewReport/Report/ReportView.js
+++ src/Components/ViewReport/Report/ReportView.js
@@ -50,7 +50,7 @@ const ReportView = (props) => {
           )
           .then(() => {
             const fatchFile = new FormData();
-            fatchFile.append("reportFile");
+            // fatchFile.append("reportFile");
             axios.put(`${FileURL}/report/${fileId}`, fatchFile, {
               headers: {
                 "Content-Type": "multipart/form-data", // multipart = 파일 업로드
@@ -118,7 +118,7 @@ const ReportView = (props) => {
             }
           });
         break;
-      case "CIRCLE":
+      case "CIRCLES":
         axios
           .patch(
             `${MainURL}/report/team/${reportId}`,
